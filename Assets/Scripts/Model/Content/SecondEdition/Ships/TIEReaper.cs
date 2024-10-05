7bda3d409826993e3f9dac938bc60681cdcbd673
Phil Pursglove <philpursglove@users.noreply.github.com>
2024-10-05 23:35:47 +0100

    Replaced AdvancedAilerons with COntrolledAilerons

--- Assets/Scripts/Model/Content/SecondEdition/Ships/TIEReaper.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/TIEReaper.cs
@@ -6,6 +6,10 @@ using Arcs;
 using Upgrade;
 using System;
 using Ship.CardInfo;
+using Ship;
+using Tokens;
+using System.Linq;
+using SubPhases;
 
 namespace Ship
 {
@@ -37,7 +41,7 @@ namespace Ship
                     new ShipUpgradesInfo()
                 );
 
-                ShipAbilities.Add(new Abilities.SecondEdition.AdvancedAileronsAbility());
+                ShipAbilities.Add(new Abilities.SecondEdition.ControlledAileronsAbility());
 
                 ModelInfo = new ShipModelInfo
                 (
@@ -92,23 +96,159 @@ namespace Ship
 
 namespace Abilities.SecondEdition
 {
-    public class AdvancedAileronsAbility : AdaptiveAileronsAbility
+    public class ControlledAileronsAbility : GenericAbility
     {
+        private GenericMovement SavedManeuver;
+
+        private static readonly List<string> ChangedManeuversCodes = new List<string>() { "1.L.B", "1.F.S", "1.R.B" };
+        private Dictionary<string, MovementComplexity> SavedManeuverColors;
+        bool doAilerons = false;
 
         public override void ActivateAbility()
         {
-            base.ActivateAbility();
-
-            HostShip.DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Bank), MovementComplexity.Normal);
-            HostShip.DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Bank), MovementComplexity.Normal);
+            HostShip.OnManeuverIsReadyToBeRevealed += RegisterControlledAileronsAbility;
         }
 
         public override void DeactivateAbility()
         {
-            base.ActivateAbility();
-
-            HostShip.DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed3, ManeuverDirection.Left, ManeuverBearing.Bank), MovementComplexity.Complex);
-            HostShip.DialInfo.ChangeManeuverComplexity(new ManeuverHolder(ManeuverSpeed.Speed3, ManeuverDirection.Right, ManeuverBearing.Bank), MovementComplexity.Complex);
+            HostShip.OnManeuverIsReadyToBeRevealed -= RegisterControlledAileronsAbility;
         }
+
+        private void RegisterControlledAileronsAbility(GenericShip ship)
+        {
+            // AI doesn't know how to boost
+            if (HostShip.Owner.GetType().IsSubclassOf(typeof(Players.GenericAiPlayer))) return;
+
+            RegisterAbilityTrigger(TriggerTypes.OnManeuverIsReadyToBeRevealed, CheckCanUseAbility);
+        }
+
+        private void CheckCanUseAbility(object sender, EventArgs e)
+        {
+            if (HostShip.Tokens.HasToken(typeof(StressToken)))
+            {
+                Triggers.FinishTrigger();
+            }
+            else
+            {
+                DoControlledAileronsAbility();
+            }
+        }
+
+        private void DoControlledAileronsAbility()
+        {
+            SavedManeuver = HostShip.AssignedManeuver;
+
+            SavedManeuverColors = new Dictionary<string, MovementComplexity>();
+            foreach (var changedManeuver in ChangedManeuversCodes)
+            {
+                SavedManeuverColors.Add(changedManeuver, HostShip.DialInfo.PrintedDial.First(n => n.Key.ToString() == changedManeuver).Value);
+                HostShip.Maneuvers[changedManeuver] = MovementComplexity.Normal;
+            }
+
+            Triggers.RegisterTrigger(
+                new Trigger()
+                {
+                    Name = "Ailerons Planning",
+                    TriggerType = TriggerTypes.OnAbilityDirect,
+                    TriggerOwner = Selection.ThisShip.Owner.PlayerNo,
+                    EventHandler = AskToUseControlledAilerons
+                }
+            );
+
+            Triggers.ResolveTriggers(TriggerTypes.OnAbilityDirect, ExecuteSelectedManeuver);
+        }
+
+        private void AskToUseControlledAilerons(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                NeverUseByDefault,
+                UseAilerons,
+                DontUseAilerons,
+                descriptionLong: "Do you want to activate your Controlled Ailerons?",
+                imageHolder: HostShip
+            );
+        }
+
+        private void DontUseAilerons(object sender, EventArgs e)
+        {
+            DecisionSubPhase.ConfirmDecision();
+        }
+
+        private void UseAilerons(object sender, EventArgs e)
+        {
+            DecisionSubPhase.ConfirmDecisionNoCallback();
+            SelectAdaptiveAileronsManeuver(sender, e);
+        }
+
+        private void SelectAdaptiveAileronsManeuver(object sender, EventArgs e)
+        {
+            doAilerons = true;
+            HostShip.Owner.ChangeManeuver(
+                ShipMovementScript.SendAssignManeuverCommand,
+                Triggers.FinishTrigger,
+                AdaptiveAileronsFilter
+            );
+        }
+
+        private void RestoreManeuverColors(GenericShip ship)
+        {
+            foreach (var changedManeuver in ChangedManeuversCodes)
+            {
+                HostShip.Maneuvers[changedManeuver] = SavedManeuverColors[changedManeuver];
+            }
+        }
+
+        private void ExecuteSelectedManeuver()
+        {
+            if (doAilerons)
+            {
+                HostShip.AssignedManeuver.IsRevealDial = false;
+                HostShip.AssignedManeuver.GrantedBy = "Ailerons";
+                HostShip.CanPerformActionsWhenBumped = true;
+                HostShip.CanPerformActionsWhenOverlapping = true;
+                ShipMovementScript.LaunchMovement(FinishAdaptiveAileronsAbility);
+            }
+            else
+            {
+                FinishAdaptiveAileronsAbility();
+            }
+        }
+
+        private void FinishAdaptiveAileronsAbility()
+        {
+            doAilerons = false;
+            HostShip.CanPerformActionsWhenBumped = false;
+            HostShip.CanPerformActionsWhenOverlapping = false;
+            RestoreManeuverColors(HostShip);
+            Phases.CurrentSubPhase.IsReadyForCommands = true;
+            //ship may have flown off the board; only assign saved maneuver if ship is exists
+            if (Roster.GetShipById("ShipId:" + Selection.ThisShip.ShipId) != null)
+            {
+                ManeuverSelectionSubphase subphase = Phases.StartTemporarySubPhaseNew<ManeuverSelectionSubphase>(
+                    "Select a maneuver",
+                    Triggers.FinishTrigger
+                );
+                subphase.RequiredPlayer = Selection.ThisShip.Owner.PlayerNo;
+                subphase.Start();
+                subphase.IsReadyForCommands = true;
+
+                ShipMovementScript.AssignManeuver(Selection.ThisShip.ShipId, SavedManeuver.ToString());
+            }
+            else
+            {
+                Triggers.FinishTrigger();
+            }
+        }
+
+        private bool AdaptiveAileronsFilter(string maneuverString)
+        {
+            GenericMovement movement = ShipMovementScript.MovementFromString(maneuverString, HostShip);
+            if (movement.ManeuverSpeed != ManeuverSpeed.Speed1) return false;
+            if (movement.Bearing == ManeuverBearing.Straight || movement.Bearing == ManeuverBearing.Bank) return true;
+
+            return false;
+        }
+
     }
 }
