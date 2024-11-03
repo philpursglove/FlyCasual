ccae59d49a2600ee53345a59d8837f3134b74588
Jeremy S <baledin@gmail.com>
2024-11-03 09:50:28 -0800

    Imported Wolffe - Siege of Coruscant from Legacy. Minor cleanup.

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Republic/ARC170Starfighter/WolffeSoC.cs
@@ -0,0 +1,53 @@
+﻿using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using Content;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.ARC170Starfighter
+    {
+        public class WolffeSoC : ARC170Starfighter
+        {
+            public WolffeSoC() : base()
+            {
+                PilotInfo = new PilotCardInfo25(
+                    "\"Wolffe\"",
+                    "Siege of Coruscant",
+                    Faction.Republic,
+                    4,
+                    4,
+                    0,
+                    isLimited: true,
+                    abilityType: typeof(Abilities.SecondEdition.WolffeAbility),
+                    charges: 1,
+                    extraUpgradeIcons: new List<UpgradeType>
+                    { 
+                        UpgradeType.Crew,
+                        UpgradeType.Gunner,
+                        UpgradeType.Gunner,
+                        UpgradeType.Astromech
+                    },
+                    tags: new List<Tags>
+                    {
+                        Tags.Clone
+                    },
+                    isStandardLayout: true
+                    
+                );
+
+                MustHaveUpgrades.Add(typeof(UpgradesList.SecondEdition.Wolfpack));
+                MustHaveUpgrades.Add(typeof(UpgradesList.SecondEdition.VeteranTailGunner));
+                MustHaveUpgrades.Add(typeof(UpgradesList.SecondEdition.Q7Astromech));
+
+                ShipAbilities.Add(new Abilities.SecondEdition.BornForThisAbility());
+
+                PilotNameCanonical = "wolffe-siegeofcoruscant";
+
+                ModelInfo.SkinName = "Wolffe";
+            }
+        }
+    }
+}
