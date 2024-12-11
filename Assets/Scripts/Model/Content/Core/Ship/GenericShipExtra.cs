f92bb10efb0a6c464bc220c1c379de2edd50815a
Jeremy S <baledin@gmail.com>
2024-12-10 16:27:20 -0800

    Initial work for Electro-Chaff missiles, cloud and missiles not working correctly at this time. Clouds can likely utilize BreakAllLocks from GasClouds?

--- Assets/Scripts/Model/Content/Core/Ship/GenericShipExtra.cs
+++ Assets/Scripts/Model/Content/Core/Ship/GenericShipExtra.cs
@@ -60,6 +60,8 @@ namespace Ship
 
         public event EventHandlerBool OnCheckCanUseForceNow;
 
+        public event EventHandlerShipToken OnRedTokenGainedFromOverlappingObstacle;
+
         public GenericShip DockingHost;
 
         public List<GenericShip> TwoTargetLocksOnDifferentTargetsAreAllowed = new List<GenericShip>();
@@ -480,6 +482,13 @@ namespace Ship
             bool canUseForceNow = true;
             OnCheckCanUseForceNow?.Invoke(ref canUseForceNow);
             return canUseForceNow;
+        }
+
+        public void CallOnRedTokenGainedFromOverlappingObstacle(GenericToken token, Action callback)
+        {
+            if (OnRedTokenGainedFromOverlappingObstacle != null) OnRedTokenGainedFromOverlappingObstacle(this, token);
+
+            Triggers.ResolveTriggers(TriggerTypes.OnRedTokenGainedFromOverlappingObstacle, callback);
         }
     }
 
