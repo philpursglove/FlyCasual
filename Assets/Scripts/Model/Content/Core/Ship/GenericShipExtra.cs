6c64425a26166dcf8d1ee6ac9ca349cfe0bc0dcb
Jeremy S <baledin@gmail.com>
2024-12-10 17:30:06 -0800

    Reverted GenericShipExtra, these can be worked with current triggers and events

--- Assets/Scripts/Model/Content/Core/Ship/GenericShipExtra.cs
+++ Assets/Scripts/Model/Content/Core/Ship/GenericShipExtra.cs
@@ -60,8 +60,6 @@ namespace Ship
 
         public event EventHandlerBool OnCheckCanUseForceNow;
 
-        public event EventHandlerShipToken OnRedTokenGainedFromOverlappingObstacle;
-
         public GenericShip DockingHost;
 
         public List<GenericShip> TwoTargetLocksOnDifferentTargetsAreAllowed = new List<GenericShip>();
@@ -482,13 +480,6 @@ namespace Ship
             bool canUseForceNow = true;
             OnCheckCanUseForceNow?.Invoke(ref canUseForceNow);
             return canUseForceNow;
-        }
-
-        public void CallOnRedTokenGainedFromOverlappingObstacle(GenericToken token, Action callback)
-        {
-            if (OnRedTokenGainedFromOverlappingObstacle != null) OnRedTokenGainedFromOverlappingObstacle(this, token);
-
-            Triggers.ResolveTriggers(TriggerTypes.OnRedTokenGainedFromOverlappingObstacle, callback);
         }
     }
 
