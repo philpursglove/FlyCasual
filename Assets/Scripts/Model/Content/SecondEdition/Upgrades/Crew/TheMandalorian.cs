0ab21d3980b6b56591746cf57434869c13d7e932
Jeremy S <baledin@gmail.com>
2024-12-24 15:20:26 -0800

    Imported The Mandalorian crew upgrade from Legacy, adjusted points to current AMG release

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/TheMandalorian.cs
@@ -0,0 +1,58 @@
+﻿using Actions;
+using ActionsList;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class TheMandalorian : GenericUpgrade
+    {
+        public TheMandalorian() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "The Mandalorian",
+                UpgradeType.Crew,
+                cost: 2,
+                isLimited: true,
+                restriction: new FactionRestriction(Faction.Scum),
+                addAction: new ActionInfo(typeof(ReinforceAction)),
+                abilityType: typeof(Abilities.SecondEdition.TheMandalorianCrewAbility)
+            );
+        }        
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class TheMandalorianCrewAbility : GenericAbility
+    {
+        //During the End Phase, if you did not defend this round, recover 1 non-recurring , if able.
+        bool didDefend = false;
+
+        public override void ActivateAbility()
+        {
+            HostShip.OnDefenceStartAsDefender += RegisterDidDefend;
+            Phases.Events.OnEndPhaseStart_Triggers += RecoverForce;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnDefenceStartAsDefender -= RegisterDidDefend;
+            Phases.Events.OnEndPhaseStart_Triggers -= RecoverForce;
+        }
+
+        private void RegisterDidDefend()
+        {
+            didDefend = true;
+        }
+
+        private void RecoverForce()
+        {
+            if(!didDefend && !HostShip.IsForceRecurring && HostShip.State.Force < HostShip.State.MaxForce)
+            {
+                Messages.ShowInfo($"{HostUpgrade.UpgradeInfo.Name}: {HostShip.PilotInfo.PilotName} recovers 1 Force");
+                HostShip.State.RestoreForce();
+            }
+            didDefend = false;
+        }
+    }
+}
