f97c2cd6ae3d46dffd361319447fe6bf161a0d6d
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-01-01 23:31:01 +0000

    Add Blank Signature

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Sensor/BlankSignature.cs
@@ -0,0 +1,20 @@
+﻿using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class BlankSignature : GenericUpgrade
+    {
+        public BlankSignature()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Blank Signature",
+                UpgradeType.Sensor,
+                regensCharges: true,
+                charges:1
+            );
+            IsHidden = true;
+
+        }
+
+    }
+}
