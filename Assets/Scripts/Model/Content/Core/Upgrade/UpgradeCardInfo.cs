493678369091d0cc3f17098c4aa2d74fc7596aaa
Jeremy S <baledin@gmail.com>
2025-08-05 16:44:07 -0700

    Set upgrades to include XWA by default.

--- Assets/Scripts/Model/Content/Core/Upgrade/UpgradeCardInfo.cs
+++ Assets/Scripts/Model/Content/Core/Upgrade/UpgradeCardInfo.cs
@@ -147,7 +147,7 @@ namespace Upgrade
             SubType = subType;
             RemoteType = remoteType;
 
-            LegalityInfo = legalityInfo ?? new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal };
+            LegalityInfo = legalityInfo ?? new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal, Legality.XWA };
         }
 
         public bool HasType(UpgradeType upgradeType)
