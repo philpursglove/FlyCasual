0e9ac6b7fc25e7da7715d6c3a00ebc7445a75204
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-08-17 22:53:12 +0100

    Copy in from Legacy and tidy up

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIEInterceptor/MausMonareBoE.cs
@@ -0,0 +1,66 @@
+﻿using Abilities.SecondEdition;
+using ActionsList;
+using System;
+using System.Linq;
+using Tokens;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEInterceptor
+    {
+        public class MausMonareBoE : TIEInterceptor
+        {
+            public MausMonareBoE()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Maus Monare",
+                    3,
+                    42,
+                    isLimited: true,
+                    abilityType: typeof(MausMonareAbility),
+                    extraUpgradeIcon: UpgradeType.Talent
+                );
+                PilotNameCanonical = "mausmonare-battleoverendor";
+                ShipInfo.Shields++;
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Modification);
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Configuration);
+                AutoThrustersAbility oldAbility = (AutoThrustersAbility)ShipAbilities.First(n => n.GetType() == typeof(AutoThrustersAbility));
+                ShipAbilities.Remove(oldAbility);
+                ShipAbilities.Add(new SensitiveControlsRealAbility());
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //After you perform an evade action, gain a calculate token.
+    public class MausMonareAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += CheckCalculateBonus;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= CheckCalculateBonus;
+        }
+
+        private void CheckCalculateBonus(GenericAction action)
+        {
+            if (action is EvadeAction)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnActionIsPerformed, AssignBonusCalculateToken);
+            }
+        }
+        private void AssignBonusCalculateToken(object sender, EventArgs e)
+        {
+            Messages.ShowInfo($"{HostShip.PilotInfo.PilotName} gains Calculate token");
+
+            HostShip.Tokens.AssignToken(typeof(CalculateToken), Triggers.FinishTrigger);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIEInterceptor/Sapphire2BoE.cs
@@ -0,0 +1,55 @@
+﻿using Abilities.SecondEdition;
+using System.Linq;
+using Tokens;
+using Upgrade;
+
+namespace Ship
+{
+    namespace SecondEdition.TIEInterceptor
+    {
+        public class Sapphire2BoE : TIEInterceptor
+        {
+            public Sapphire2BoE()
+            {
+                PilotInfo = new PilotCardInfo(
+                    "Sapphire 2",
+                    1,
+                    38,
+                    isLimited: true,
+                    abilityType: typeof(Sapphire2Ability)
+                );
+                PilotNameCanonical = "sapphire2-battleoverendor";
+                ShipInfo.UpgradeIcons.Upgrades.Remove(UpgradeType.Configuration);
+                AutoThrustersAbility oldAbility = (AutoThrustersAbility)ShipAbilities.First(n => n.GetType() == typeof(AutoThrustersAbility));
+                ShipAbilities.Remove(oldAbility);
+                ShipAbilities.Add(new SensitiveControlsRealAbility());
+            }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //While you defend, if you are focused, roll 1 additional defense die.
+    public class Sapphire2Ability : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.AfterGotNumberOfDefenceDice += CheckDefenseBonus;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.AfterGotNumberOfDefenceDice -= CheckDefenseBonus;
+        }
+
+        private void CheckDefenseBonus(ref int count)
+        {
+            if (HostShip.Tokens.HasToken(typeof(FocusToken)))
+            {
+                Messages.ShowInfo($"{HostShip.PilotInfo.PilotName} rolls 1 additional defense die");
+                count++;
+            }
+        }
+    }
+}
