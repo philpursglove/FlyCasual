e8d8618eb8019053a276fad83fe02a480104f6bc
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-08-25 14:33:49 +0100

    Scimitar 3

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIESaBomber/Scimitar3BoE.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIESaBomber/Scimitar3BoE.cs
@@ -1,4 +1,7 @@
-﻿using Content;
+﻿using ActionsList;
+using Bombs;
+using Content;
+using System;
 using System.Collections.Generic;
 using Upgrade;
 using UpgradesList.SecondEdition;
@@ -9,16 +12,18 @@ namespace Ship
     {
         public class Scimitar3BoE : TIESaBomber
         {
-            public Scimitar3BoE() : base()
+            public Scimitar3BoE()
             {
                 PilotInfo = new PilotCardInfo25(
                     "Scimitar 3",
+                    "Battle Over Endor",
+                    Faction.Imperial,
                     4,
-                    57,
+                    4,
+                    0,
                     tags: new List<Tags>
                     {
-                        Tags.BoE,
-                        Tags.SL
+                        Tags.Tie,
                     },
                     isStandardLayout: true,
                     isLimited: true,
@@ -27,17 +32,76 @@ namespace Ship
                     extraUpgradeIcons: new List<UpgradeType>()
                     {
                         UpgradeType.Talent,
-                        UpgradeType.Talent
+                        UpgradeType.Talent,
+                        UpgradeType.Torpedo,
+                        UpgradeType.Device
                     }
                 );
 
-                MustHaveUpgrades.Add(typeof(NoEscapeBoE));
-                MustHaveUpgrades.Add(typeof(PartingGiftBoE));
+                MustHaveUpgrades.Add(typeof(NoEscape));
+                MustHaveUpgrades.Add(typeof(PartingGift));
                 MustHaveUpgrades.Add(typeof(ProtonTorpedoes));
                 MustHaveUpgrades.Add(typeof(ProtonBombs));
 
                 PilotNameCanonical = "scimitar3-battleoverendor";
+                ImageUrl = "https://infinitearenas.com/xw2/images/quickbuilds/scimitar3-battleoverendor.png";
             }
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //After you drop a bomb, you may spend 1 charge to perform a Boost action.
+    public class Scimitar3Ability : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnBombWasDropped += OnDeviceDropped;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnBombWasDropped -= OnDeviceDropped;
+        }
+
+        private void OnDeviceDropped()
+        {
+            if (HostShip.State.Charges > 0 && BombsManager.CurrentDevice.UpgradeInfo.SubType == UpgradeSubType.Bomb)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnBombWasDropped, AskUseAbility);
+            }
+
+        }
+
+        private void AskUseAbility(object sender, EventArgs e)
+        {
+            HostShip.BeforeActionIsPerformed += RegisterSpendChargeTrigger;
+            HostShip.AskPerformFreeAction(
+                new BoostAction(),
+                CleanUp,
+                HostShip.PilotInfo.PilotName,
+                "After you drop a bomb, you may spend 1 Charge to perform a Boost action.",
+                HostShip
+            );
+        }
+
+        private void RegisterSpendChargeTrigger(GenericAction action, ref bool isFreeAction)
+        {
+            HostShip.BeforeActionIsPerformed -= RegisterSpendChargeTrigger;
+            RegisterAbilityTrigger(
+                TriggerTypes.OnFreeAction,
+                delegate {
+                    HostShip.SpendCharge();
+                    Triggers.FinishTrigger();
+                }
+            );
+        }
+
+        private void CleanUp()
+        {
+            HostShip.BeforeActionIsPerformed -= RegisterSpendChargeTrigger;
+            Triggers.FinishTrigger();
         }
     }
 }
