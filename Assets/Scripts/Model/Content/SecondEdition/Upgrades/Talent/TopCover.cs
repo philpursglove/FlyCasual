c5d04f51f198e417550bdc9d9fa05bcf7c472051
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-08-31 23:35:30 +0100

    Skeleton Top Cover

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/TopCover.cs
@@ -0,0 +1,36 @@
+﻿using Abilities.SecondEdition;
+using System;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class TopCover : GenericUpgrade
+    {
+        public TopCover()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                name: "Top Cover",
+                type: UpgradeType.Talent,
+                cost: 0,
+                abilityType: typeof(TopCoverAbility));
+            IsHidden = true;
+            IsWIP = true;
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class TopCoverAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void DeactivateAbility()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
