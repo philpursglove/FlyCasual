0cc85293beb58190e9fbe146ea80700e9adc491a
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-09-01 13:35:43 +0100

    Draft of Top Cover

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/TopCover.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/TopCover.cs
@@ -1,5 +1,10 @@
 ﻿using Abilities.SecondEdition;
-using System;
+using BoardTools;
+using Bombs;
+using Movement;
+using Ship;
+using SubPhases;
+using System.Collections.Generic;
 using Upgrade;
 
 namespace UpgradesList.SecondEdition
@@ -25,12 +30,63 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-            throw new NotImplementedException();
+            GenericShip.OnAttackFinishGlobal += CheckAbility;
         }
 
         public override void DeactivateAbility()
         {
-            throw new NotImplementedException();
+            GenericShip.OnAttackFinishGlobal -= CheckAbility;
         }
+
+        private void CheckAbility(GenericShip ship)
+        {
+            if (Tools.IsFriendly(ship, HostShip)
+                && Board.IsShipBetweenRange(HostShip, ship, 0, 3)
+                && (Combat.Defender != null)
+                && Tools.IsSameShip(ship, Combat.Defender)
+               // Needs a condition about whether a device has been dropped/launched
+               )
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnAttackFinish, AskUseAbility);
+            }
+        }
+
+        private void AskUseAbility(object sender, System.EventArgs e)
+        {
+            AskToUseAbility
+            (
+                descriptionShort: "Top Cover",
+                descriptionLong: "Do you want to launch a bomb using the [3] straight or bank template?",
+                useByDefault: NeverUseByDefault,
+                useAbility: DropBomb,
+                imageHolder: HostUpgrade
+            );
+        }
+
+        private void DropBomb(object sender, System.EventArgs e)
+        {
+            DecisionSubPhase.ConfirmDecisionNoCallback();
+
+            HostShip.OnGetAvailableBombLaunchTemplates += AddLaunchTemplates;
+
+            BombsManager.RegisterBombDropTriggerIfAvailable(
+                HostShip,
+                TriggerTypes.OnAbilityDirect,
+                subType: UpgradeSubType.Bomb,
+                isRealDrop: false
+            );
+
+            Triggers.ResolveTriggers(TriggerTypes.OnAbilityDirect, Triggers.FinishTrigger);
+        }
+
+        protected virtual void AddLaunchTemplates(List<ManeuverTemplate> availableTemplates, GenericUpgrade upgrade)
+        {
+            if (upgrade.UpgradeInfo.SubType != UpgradeSubType.Bomb) return;
+            availableTemplates.Clear();
+            availableTemplates.Add(new ManeuverTemplate(ManeuverBearing.Straight, ManeuverDirection.Forward, ManeuverSpeed.Speed3));
+            availableTemplates.Add(new ManeuverTemplate(ManeuverBearing.Bank, ManeuverDirection.Left, ManeuverSpeed.Speed3));
+            availableTemplates.Add(new ManeuverTemplate(ManeuverBearing.Bank, ManeuverDirection.Right, ManeuverSpeed.Speed3));
+        }
+
     }
 }
