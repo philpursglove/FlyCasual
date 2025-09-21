8a6ae1b61f471035cd969063432d9c44e713ea4c
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-09-21 11:40:05 +0100

    Skeleton Phantoms

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIEPhPhantom/EchoSL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship.SecondEdition.TIEPhPhantom
+{
+    public class EchoSL : TIEPhPhantom
+    {
+        public EchoSL() : base()
+        {
+            PilotInfo = new PilotCardInfo25
+            (
+                "\"Echo\" (SL)",
+                "Copycat",
+                Faction.Imperial,
+                4,
+                5,
+                9,
+                isLimited: true,
+                abilityType: typeof(Abilities.SecondEdition.EchoSLAbility),
+                tags: new List<Tags>
+                {
+                    Tags.Tie
+                },
+                extraUpgradeIcons: new List<UpgradeType>()
+                {
+                    UpgradeType.Talent,
+                    UpgradeType.Talent,
+                    UpgradeType.Sensor,
+                    UpgradeType.Gunner,
+                    UpgradeType.Modification
+                },
+                seImageNumber: 134,
+                legality: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
+            );
+            ModelInfo.SkinName = "Echo SL";
+        }
+    }
+    {
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIEPhPhantom/WhisperSL.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ship.SecondEdition.TIEPhPhantom
+{
+    public class WhisperSL : TIEPhPhantom
+    {
+        public WhisperSL() : base()
+        {
+            PilotInfo = new PilotCardInfo25
+            (
+                "Whisper (SL)",
+                "Unseen Assailant",
+                Faction.Imperial,
+                5,
+                5,
+                9,
+                isLimited: true,
+                abilityType: typeof(Abilities.SecondEdition.WhisperSLAbility),
+                tags: new List<Tags>
+                {
+                    Tags.Tie
+                },
+                extraUpgradeIcons: new List<UpgradeType>()
+                {
+                    UpgradeType.Talent,
+                    UpgradeType.Talent,
+                    UpgradeType.Sensor,
+                    UpgradeType.Gunner,
+                    UpgradeType.Modification
+                },
+                seImageNumber: 133,
+                legality: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
+            );
+            ModelInfo.SkinName = "Whisper SL";
+        }
+    }
+    {
+    }
+}
