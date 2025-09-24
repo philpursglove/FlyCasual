7a61ad3e36f2bf227c5571128c8b42bca4fe6d8a
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-09-24 23:15:12 +0100

    Fix method name

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIEPhPhantom/Whisper.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIEPhPhantom/Whisper.cs
@@ -67,10 +67,10 @@ namespace Abilities.SecondEdition
 
         public void RegisterWhisperAbility()
         {
-            RegisterAbilityTrigger(TriggerTypes.OnAttackHit, AskAssignFocus);
+            RegisterAbilityTrigger(TriggerTypes.OnAttackHit, AskAssignEvade);
         }
 
-        private void AskAssignFocus(object sender, System.EventArgs e)
+        private void AskAssignEvade(object sender, System.EventArgs e)
         {
             if (!alwaysUseAbility)
             {
