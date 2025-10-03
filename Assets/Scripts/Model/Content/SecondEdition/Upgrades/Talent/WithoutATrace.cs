d265fa0d6762febd2c5e3379d86669f91a7a784e
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-03 23:52:41 +0100

    WIP

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/WithoutATrace.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/WithoutATrace.cs
@@ -1,3 +1,9 @@
+using RulesList;
+using Ship;
+using SubPhases;
+using System;
+using System.Linq;
+using Tokens;
 using Upgrade;
 
 namespace UpgradesList.SecondEdition
@@ -21,9 +27,74 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
+            HostShip.OnTokenIsAssigned += RegisterAbility;
+
+            TargetLocksRule.OnCheckTargetLockIsDisallowed += ForbidLockOnMe;
+
         }
         public override void DeactivateAbility()
         {
+            HostShip.OnTokenIsAssigned -= RegisterAbility;
+
+            TargetLocksRule.OnCheckTargetLockIsDisallowed -= ForbidLockOnMe;
+
+        }
+
+        private void ForbidLockOnMe(ref bool isAllowed, GenericShip lockSource, ITargetLockable lockTarget)
+        {
+            if (lockTarget is GenericShip && (lockTarget as GenericShip) == HostShip
+                                          && HostShip.IsCloaked)
+            {
+                isAllowed = false;
+            }
+        }
+
+
+        private void RegisterAbility(GenericShip ship, GenericToken token)
+        {
+            if (token is CloakToken && HostShip.Tokens.HasTokenByColor(TokenColors.Red))
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnTokenIsAssigned, AskToUseAbility);
+            }
+        }
+
+        private void AskToUseAbility(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                AlwaysUseByDefault,
+                UseAbility,
+                descriptionLong: "After cloaking you may remove a red token",
+                imageHolder: HostShip
+            );
+        }
+
+        private void UseAbility(object sender, EventArgs e)
+        {
+            var subphase = Phases.StartTemporarySubPhaseNew<WithoutATraceRemoveRedTokenAbilityDecisionSubPhase>(
+                "Without A Trace: You may remove 1 red token",
+                Triggers.FinishTrigger
+            );
+            subphase.ImageSource = HostShip;
+            subphase.AbilityHostShip = HostShip;
+            subphase.Start();
+        }
+    }
+}
+
+namespace SubPhases
+{
+    public class WithoutATraceRemoveRedTokenAbilityDecisionSubPhase : RemoveRedTokenDecisionSubPhase
+    {
+        public GenericShip AbilityHostShip;
+
+        public override void PrepareCustomDecisions()
+        {
+            DescriptionShort = "Without A Trace";
+            DescriptionLong = "You may remove 1 red token";
+
+            DecisionOwner = Selection.ThisShip.Owner;
+            DefaultDecisionName = decisions.First().Name;
         }
     }
 }
