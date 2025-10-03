ab07ec497736797f76eda0ede093531fb0de52f5
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-03 23:52:41 +0100

    WIP

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Sensor/RelaySystem.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Sensor/RelaySystem.cs
@@ -1,3 +1,6 @@
+using System;
+using Ship;
+using Tokens;
 using Upgrade;
 
 namespace UpgradesList.SecondEdition
@@ -22,9 +25,47 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
+            GenericShip.OnTokenIsAssignedGlobal += CheckReceiveTargetLockAbility;
+
+            HostShip.OnAttackHitAsAttacker += CheckPassOnTargetLockAbility;
         }
+
+        private void CheckPassOnTargetLockAbility()
+        {
+        }
+
         public override void DeactivateAbility()
         {
+            GenericShip.OnTokenIsAssignedGlobal -= CheckReceiveTargetLockAbility;
+
+            HostShip.OnAttackHitAsAttacker -= CheckPassOnTargetLockAbility;
+        }
+
+        private ITargetLockable _lockedObject;
+
+        private void CheckReceiveTargetLockAbility(GenericShip ship, GenericToken token)
+        {
+            if ((token is BlueTargetLockToken) && (ship.Owner.PlayerNo == HostShip.Owner.PlayerNo) &&
+                (ship.GetRangeToShip(HostShip) < 3))
+            {
+                _lockedObject = ((BlueTargetLockToken)token).OtherTargetLockTokenOwner;
+
+                AskToUseAbility("Relay System",
+                    NeverUseByDefault,
+                    descriptionLong: $"You may acquire a lock",
+                    useAbility: AcquireTargetLock);
+            }
+        }
+
+        private void AcquireTargetLock(object sender, EventArgs e)
+        {
+            if (HostShip.Tokens.HasToken<BlueTargetLockToken>())
+            {
+                BlueTargetLockToken existingToken = (BlueTargetLockToken)HostShip.Tokens.GetToken(typeof(BlueTargetLockToken));
+                HostShip.RemoveToken(existingToken);
+            }
+
+            ActionsHolder.AcquireTargetLock(HostShip, _lockedObject, () => { }, () => { }, false);
         }
     }
 }
