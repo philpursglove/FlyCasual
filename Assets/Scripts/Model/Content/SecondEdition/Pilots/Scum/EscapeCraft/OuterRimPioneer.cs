f8215842368fa2f786d3248c92bd5cf52d4ef923
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-19 22:12:16 +0100

    Escape craft

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/EscapeCraft/OuterRimPioneer.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/EscapeCraft/OuterRimPioneer.cs
@@ -40,9 +40,15 @@ namespace Ship
         {
             public OuterRimPioneerXWA() : base()
             {
-                (PilotInfo as PilotCardInfo25).Cost = 3;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 11;
+                (PilotInfo as PilotCardInfo25).Cost = 7;
+                (PilotInfo as PilotCardInfo25).LoadoutValue = 9;
                 (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
+                (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
+                {
+                    UpgradeType.Crew,
+                    UpgradeType.Modification,
+                    UpgradeType.Illicit
+                };
             }
         }
     }
