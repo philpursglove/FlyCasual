7c01b7aa828e44c067fcc1c8b954aa03ef2d6b1a
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-19 23:43:47 +0100

    Scurrg

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/ScurrgH6Bomber/CaptainNym.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/ScurrgH6Bomber/CaptainNym.cs
@@ -46,8 +46,8 @@ namespace Ship
         {
             public CaptainNymXWA() : base()
             {
-                (PilotInfo as PilotCardInfo25).Cost = 5;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 18;
+                (PilotInfo as PilotCardInfo25).Cost = 14;
+                (PilotInfo as PilotCardInfo25).LoadoutValue = 22;
                 (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
             }
         }
