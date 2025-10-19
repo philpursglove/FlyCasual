c4806d33f26aaa6f9e37908ab6c0ed9bafda1670
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-19 23:10:00 +0100

    Jumpmaster

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/JumpMaster5000/Dengar.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/JumpMaster5000/Dengar.cs
@@ -50,8 +50,8 @@ namespace Ship
         {
             public DengarXWA() : base()
             {
-                (PilotInfo as PilotCardInfo25).Cost = 6;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 14;
+                (PilotInfo as PilotCardInfo25).Cost = 18;
+                (PilotInfo as PilotCardInfo25).LoadoutValue = 18;
                 (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
             }
         }
--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/JumpMaster5000/Manaroo.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/JumpMaster5000/Manaroo.cs
@@ -52,8 +52,8 @@ namespace Ship
         {
             public ManarooXWA() : base()
             {
-                (PilotInfo as PilotCardInfo25).Cost = 5;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 18;
+                (PilotInfo as PilotCardInfo25).Cost = 12;
+                (PilotInfo as PilotCardInfo25).LoadoutValue = 13;
                 (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
             }
         }
