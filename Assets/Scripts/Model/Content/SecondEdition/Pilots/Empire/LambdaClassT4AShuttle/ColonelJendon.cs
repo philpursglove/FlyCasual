0c9f4db5531a4f9e509d5e33b9758a3838d78775
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-20 17:51:27 +0100

    Lambda

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/LambdaClassT4AShuttle/ColonelJendon.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/LambdaClassT4AShuttle/ColonelJendon.cs
@@ -43,15 +43,15 @@ namespace Ship
         {
             public ColonelJendonXWA() : base()
             {
-                (PilotInfo as PilotCardInfo25).Cost = 5;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 13;
+                (PilotInfo as PilotCardInfo25).Cost = 13;
+                (PilotInfo as PilotCardInfo25).LoadoutValue = 18;
                 (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
                 {
+                    UpgradeType.Talent,
                     UpgradeType.Crew,
                     UpgradeType.Crew,
                     UpgradeType.Sensor,
                     UpgradeType.Modification,
-                    UpgradeType.Cannon,
                     UpgradeType.Cannon,
                     UpgradeType.Title
                 };
