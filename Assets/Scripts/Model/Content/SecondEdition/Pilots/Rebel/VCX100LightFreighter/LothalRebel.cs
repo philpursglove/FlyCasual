84ccf48a8b96be7d813118944f911b79991dcadb
Jeremy S <baledin@gmail.com>
2025-10-23 16:35:04 -0700

    Rebel VCX-100 Light Freighter pilot point updates

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Rebel/VCX100LightFreighter/LothalRebel.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Rebel/VCX100LightFreighter/LothalRebel.cs
@@ -2,52 +2,53 @@
 using System.Collections.Generic;
 using Upgrade;
 
-namespace Ship
+namespace Ship.SecondEdition.VCX100LightFreighter
 {
-    namespace SecondEdition.VCX100LightFreighter
+    public class LothalRebel : VCX100LightFreighter
     {
-        public class LothalRebel : VCX100LightFreighter
+        public LothalRebel() : base()
         {
-            public LothalRebel() : base()
-            {
-                PilotInfo = new PilotCardInfo25
-                (
-                    "Lothal Rebel",
-                    "",
-                    Faction.Rebel,
-                    2,
-                    7,
-                    8,
-                    tags: new List<Tags>
-                    {
-                        Tags.Freighter
-                    },
-                    extraUpgradeIcons: new List<UpgradeType>
-                    {
-                        UpgradeType.Turret,
-                        UpgradeType.Torpedo,
-                        UpgradeType.Gunner
-                    },
-                    seImageNumber: 76,
-                    legality: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
-                );
-            }
-        }
-
-        public class LothalRebelXWA : LothalRebel
-        {
-            public LothalRebelXWA() : base()
-            {
-                (PilotInfo as PilotCardInfo25).Cost = 6;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 10;
-                (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
-                { 
-                    UpgradeType.Crew,
+            PilotInfo = new PilotCardInfo25
+            (
+                "Lothal Rebel",
+                "",
+                Faction.Rebel,
+                2,
+                7,
+                8,
+                tags: new List<Tags>
+                {
+                    Tags.Freighter
+                },
+                extraUpgradeIcons: new List<UpgradeType>
+                {
                     UpgradeType.Turret,
-                    UpgradeType.Torpedo
-                };
-                (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
-            }
+                    UpgradeType.Torpedo,
+                    UpgradeType.Gunner
+                },
+                seImageNumber: 76,
+                legality: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
+            );
+        }
+    }
+
+    public class LothalRebelXWA : LothalRebel
+    {
+        public LothalRebelXWA() : base()
+        {
+            (PilotInfo as PilotCardInfo25).Cost = 15;
+            (PilotInfo as PilotCardInfo25).LoadoutValue = 9;
+            (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
+            {
+                UpgradeType.Crew,
+                UpgradeType.Crew,
+                UpgradeType.Sensor,
+                UpgradeType.Gunner,
+                UpgradeType.Modification,
+                UpgradeType.Turret,
+                UpgradeType.Torpedo
+            };
+            (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
         }
     }
 }
