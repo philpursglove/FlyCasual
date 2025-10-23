7004f90f9b94c58dd4ee90ba05d63fff96b58e51
Jeremy S <baledin@gmail.com>
2025-10-23 10:35:21 -0700

    Added new XWAEdition based on SecondEdition. Added new edition to MainMenu.SetEdition and included a new SetEdition based on play Format.

---
+++ Assets/Scripts/Model/Editions/XWAEdition.cs
@@ -0,0 +1,9 @@
+namespace Editions
+{
+    public class XWAEdition : SecondEdition
+    {
+        public override int MaxPoints { get { return 50; } }
+
+        public XWAEdition() : base() { }
+    }
+}
