44358e180d61576298451982d719cddd9814dbbb
Jeremy S <baledin@gmail.com>
2025-10-23 16:37:29 -0700

    Rebel YT-2400 Light Freighter pilot point updates

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Rebel/YT2400LightFreighter/WildSpaceFringer.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Rebel/YT2400LightFreighter/WildSpaceFringer.cs
@@ -2,52 +2,50 @@
 using System.Collections.Generic;
 using Upgrade;
 
-namespace Ship
+namespace Ship.SecondEdition.YT2400LightFreighter
 {
-    namespace SecondEdition.YT2400LightFreighter
+    public class WildSpaceFringer : YT2400LightFreighter
     {
-        public class WildSpaceFringer : YT2400LightFreighter
+        public WildSpaceFringer() : base()
         {
-            public WildSpaceFringer() : base()
-            {
-                PilotInfo = new PilotCardInfo25
-                (
-                    "Wild Space Fringer",
-                    "",
-                    Faction.Rebel,
-                    1,
-                    8,
-                    7,
-                    tags: new List<Tags>
-                    {
-                        Tags.Freighter
-                    },
-                    extraUpgradeIcons: new List<UpgradeType>()
-                    {
-                        UpgradeType.Missile,
-                        UpgradeType.Illicit,
-                    },
-                    seImageNumber: 79,
-                    legality: new List<Legality>() { Legality.ExtendedLegal }
-                );
-            }
-        }
-
-        public class WildSpaceFringerXWA : WildSpaceFringer
-        {
-            public WildSpaceFringerXWA() : base()
-            {
-                (PilotInfo as PilotCardInfo25).Cost = 7;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 19;
-                (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
+            PilotInfo = new PilotCardInfo25
+            (
+                "Wild Space Fringer",
+                "",
+                Faction.Rebel,
+                1,
+                8,
+                7,
+                tags: new List<Tags>
                 {
-                    UpgradeType.Gunner,
+                    Tags.Freighter
+                },
+                extraUpgradeIcons: new List<UpgradeType>()
+                {
+                    UpgradeType.Missile,
                     UpgradeType.Illicit,
-                    UpgradeType.Modification,
-                    UpgradeType.Missile
-                };
-                (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
-            }
+                },
+                seImageNumber: 79,
+                legality: new List<Legality>() { Legality.ExtendedLegal }
+            );
+        }
+    }
+
+    public class WildSpaceFringerXWA : WildSpaceFringer
+    {
+        public WildSpaceFringerXWA() : base()
+        {
+            (PilotInfo as PilotCardInfo25).Cost = 17;
+            (PilotInfo as PilotCardInfo25).LoadoutValue = 14;
+            (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
+            {
+                UpgradeType.Crew,
+                UpgradeType.Illicit,
+                UpgradeType.Illicit,
+                UpgradeType.Modification,
+                UpgradeType.Missile
+            };
+            (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
         }
     }
 }
