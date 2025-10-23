e534a987c81469775d07e5d9ef325b1d897179b3
Jeremy S <baledin@gmail.com>
2025-10-23 15:50:01 -0700

    Added overrides for Name and NameShort

--- Assets/Scripts/Model/Editions/XWAEdition.cs
+++ Assets/Scripts/Model/Editions/XWAEdition.cs
@@ -2,6 +2,8 @@ namespace Editions
 {
     public class XWAEdition : SecondEdition
     {
+        public override string Name { get { return "XWA Edition"; } }
+        public override string NameShort { get { return "XWAEdition"; } }
         public override int MaxPoints { get { return 50; } }
 
         public XWAEdition() : base() { }
