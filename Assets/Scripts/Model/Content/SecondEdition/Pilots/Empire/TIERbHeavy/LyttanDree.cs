e9cb42433f3480e930eb95e3d8ad75c5a17834fb
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-24 22:26:21 +0100

    Fix Lyttan Dree cost & loadout

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIERbHeavy/LyttanDree.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/TIERbHeavy/LyttanDree.cs
@@ -45,8 +45,8 @@ namespace Ship
         {
             public LyttanDreeXWA() : base()
             {
-                (PilotInfo as PilotCardInfo25).Cost = 4;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 6;
+                (PilotInfo as PilotCardInfo25).Cost = 10;
+                (PilotInfo as PilotCardInfo25).LoadoutValue = 9;
                 (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
                 (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
                 {
