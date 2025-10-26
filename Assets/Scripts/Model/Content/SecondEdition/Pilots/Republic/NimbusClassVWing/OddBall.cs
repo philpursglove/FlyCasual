6e8a544a9fdbf29b2531452525216c602944c15a
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-26 15:14:33 +0000

    V-wings

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Republic/NimbusClassVWing/OddBall.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Republic/NimbusClassVWing/OddBall.cs
@@ -43,9 +43,17 @@ namespace Ship
         {
             public OddBallXWA() : base()
             {
-                (PilotInfo as PilotCardInfo25).Cost = 4;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 15;
+                (PilotInfo as PilotCardInfo25).Cost = 9;
+                (PilotInfo as PilotCardInfo25).LoadoutValue = 9;
                 (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
+                (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
+                {
+                    UpgradeType.Talent,
+                    UpgradeType.Astromech,
+                    UpgradeType.Modification,
+                    UpgradeType.Configuration
+                };
+
             }
         }
     }
