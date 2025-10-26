3c2ac1a1c883a93334a32b6430f6f02ee6b04bf4
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-26 16:25:08 +0000

    Fix Magnaguard

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Separatists/RogueClassStarfighter/MagnaGuardProtector.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Separatists/RogueClassStarfighter/MagnaGuardProtector.cs
@@ -55,7 +55,7 @@ namespace Ship
             public MagnaGuardProtectorXWA() : base()
             {
                 (PilotInfo as PilotCardInfo25).Cost = 10;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 10`;
+                (PilotInfo as PilotCardInfo25).LoadoutValue = 10;
                 (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
                 {
                     UpgradeType.Talent,
