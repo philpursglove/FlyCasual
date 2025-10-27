28767a4ec3ce21fde217450d563a1e98c62bbe24
Jeremy S <baledin@gmail.com>
2025-10-27 14:44:56 -0700

    Updated version numbers

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -12,8 +12,8 @@ public class Global : MonoBehaviour {
 
     private static bool isAlreadyInitialized;
 
-    public static string CurrentVersion = "v2.5.20250623.04";
-    public static int CurrentVersionInt = 102505004;  // literal(10) XwingVersion(25) MajorUpdate#(increments) MinorUpdate#(increments)
+    public static string CurrentVersion = "v2.5.20251026.01";
+    public static int CurrentVersionInt = 102506001;  // literal(10) XwingVersion(25) MajorUpdate#(increments) MinorUpdate#(increments)
     public static int LatestVersionInt  = 0;
 
     public static SquadBuilder SquadBuilder { get; set;}
