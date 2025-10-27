be01d1a9534d67aca362985bdbd24e64c05a82db
Jeremy S <baledin@gmail.com>
2025-10-26 21:16:26 -0700

    Cleanup

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Resistance/BTANR2WYWing/CaiThrenalli.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Resistance/BTANR2WYWing/CaiThrenalli.cs
@@ -2,48 +2,45 @@
 using System.Collections.Generic;
 using Upgrade;
 
-namespace Ship
+namespace Ship.SecondEdition.BTANR2WYWing
 {
-    namespace SecondEdition.BTANR2WYWing
+    public class CaiThrenalli : BTANR2WYWing
     {
-        public class CaiThrenalli : BTANR2WYWing
+        public CaiThrenalli() : base()
         {
-            public CaiThrenalli() : base()
-            {
-                PilotInfo = new PilotCardInfo25
-                (
-                    "C'ai Threnalli",
-                    "Tenacious Survivor",
-                    Faction.Resistance,
-                    2,
-                    10,
-                    12,
-                    isLimited: true,
-                    abilityType: typeof(Abilities.SecondEdition.CaiThrenalliAbility),
-                    extraUpgradeIcons: new List<UpgradeType>
-                    {
-                        UpgradeType.Talent,
-                        UpgradeType.Astromech,
-                        UpgradeType.Modification,
-                        UpgradeType.Modification,
-                        UpgradeType.Tech,
-                        UpgradeType.Device,
-                        UpgradeType.Turret,
-                        UpgradeType.Missile,
-                        UpgradeType.Torpedo
-                    },
-                    tags: new List<Tags>
-                    {
-                        Tags.YWing
-                    },
-                    skinName: "Red",
-                    legality: new List<Legality> { Legality.XWA }
-                );
+            PilotInfo = new PilotCardInfo25
+            (
+                "C'ai Threnalli",
+                "Tenacious Survivor",
+                Faction.Resistance,
+                2,
+                10,
+                12,
+                isLimited: true,
+                abilityType: typeof(Abilities.SecondEdition.CaiThrenalliAbility),
+                extraUpgradeIcons: new List<UpgradeType>
+                {
+                    UpgradeType.Talent,
+                    UpgradeType.Astromech,
+                    UpgradeType.Modification,
+                    UpgradeType.Modification,
+                    UpgradeType.Tech,
+                    UpgradeType.Device,
+                    UpgradeType.Turret,
+                    UpgradeType.Missile,
+                    UpgradeType.Torpedo
+                },
+                tags: new List<Tags>
+                {
+                    Tags.YWing
+                },
+                skinName: "Red",
+                legality: new List<Legality> { Legality.XWA }
+            );
 
-                PilotNameCanonical = "caithrenalli-btanr2wywing";
+            PilotNameCanonical = "caithrenalli-btanr2wywing";
 
-                ImageUrl = "https://infinitearenas.com/xw2/images/pilots/caithrenalli-btanr2ywing.png";
-            }
+            ImageUrl = "https://infinitearenas.com/xw2/images/pilots/caithrenalli-btanr2ywing.png";
         }
     }
 }
