1ae4a4ed866ed704203c2de17169c70ae9f3de73
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-10-27 13:52:39 +0000

    Add base size restriction

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/Intimidation.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/Intimidation.cs
@@ -32,6 +32,7 @@ namespace UpgradesList.SecondEdition
             UpgradeInfo.Cost = 7;
             UpgradeInfo.LegalityInfo = new List<Legality> { Legality.XWA };
             UpgradeInfo.Limited = 1;
+            UpgradeInfo.Restrictions.AddRestriction(new BaseSizeRestriction(BaseSize.Small, BaseSize.Medium));
         }
     }
 }
