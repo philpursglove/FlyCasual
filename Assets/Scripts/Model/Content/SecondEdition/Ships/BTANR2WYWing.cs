5025fed3bf43e9525d9dca9ffbefa13b971f9303
Jeremy S <baledin@gmail.com>
2025-10-27 19:53:42 -0700

    Removed Config slot and Wartime Loadout upgrade from BTA-NR2-W Y-Wing, replacing with Devastating Barrage ability; updated shields to match WL version;

--- Assets/Scripts/Model/Content/SecondEdition/Ships/BTANR2WYWing.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/BTANR2WYWing.cs
@@ -15,13 +15,12 @@ namespace Ship.SecondEdition.BTANR2WYWing
         {
             ShipCardInfo25 shipInfo = ShipInfo as ShipCardInfo25;
             shipInfo.ShipName = "BTA-NR2-W Y-wing";
-            shipInfo.UpgradeIcons.Upgrades.Add(UpgradeType.Configuration);
             shipInfo.LegalityInfo = new List<Legality>() { Legality.XWA };
             shipInfo.FactionData = new FactionData(new Dictionary<Faction, Type> { { Faction.Resistance, typeof(ZoriiBliss) } });
+            shipInfo.Shields = 5;
 
             ShipAbilities.Remove(ShipAbilities.First(n => n.GetType() == typeof(IntuitiveInterfaceAbility)));
-
-            DefaultUpgrades.Add(typeof(WartimeLoadout));
+            ShipAbilities.Add(new DevastatingBarrageAbility());
         }
     }
 }
