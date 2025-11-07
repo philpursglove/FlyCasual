a7d3f213c1b9c577519d60d79ab7fff24cbd3bfc
Jeremy S <baledin@gmail.com>
2025-11-07 11:27:58 -0800

    Moved It's a Trap to own upgrade.

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Rebel/ASF01BWing/GinaMoonsongBattleOverEndor.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Rebel/ASF01BWing/GinaMoonsongBattleOverEndor.cs
@@ -133,44 +133,6 @@ namespace Abilities.SecondEdition
         }
     }
 
-    public class ItsATrapAbility : GenericAbility
-    {
-        public override void ActivateAbility()
-        {
-            HostShip.OnAttackStartAsDefender += RegisterAbility;
-        }
-
-        public override void DeactivateAbility()
-        {
-            HostShip.OnAttackStartAsDefender -= RegisterAbility;
-        }
-
-        private void RegisterAbility()
-        {
-            AddDiceModification(
-                "It's a Trap! ability",
-                IsDiceModificationAvailable,
-                GetDiceModificationAiPriority,
-                DiceModificationType.Reroll,
-                1,
-                new List<DieSide> { DieSide.Blank }
-            );
-        }
-
-        private bool IsDiceModificationAvailable()
-        {
-            List<GenericShip> friendlyShipsInRangeOne = Board.GetShipsAtRange(HostShip, new Vector2(0, 1), Team.Type.Friendly).Where(ship => ship != HostShip).ToList();
-            List<GenericShip> enemyShipsInRangeOne = Board.GetShipsAtRange(HostShip, new Vector2(0, 1), Team.Type.Enemy);
-
-            return friendlyShipsInRangeOne.Count > enemyShipsInRangeOne.Count && Combat.DiceRollDefence.Blanks > 0;
-        }
-
-        private int GetDiceModificationAiPriority()
-        {
-            return 1000;
-        }
-    }
-
     public class GyroCockpit : GenericAbility
     {
         // After you gain a stress token, you may spend 2 charges to gain an evade token.
