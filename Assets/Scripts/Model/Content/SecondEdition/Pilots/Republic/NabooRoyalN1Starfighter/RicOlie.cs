36bbbb254bd312cb653fdbc42ec9bebb5854749b
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-12-09 20:56:19 +0000

    Ric Olie

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Republic/NabooRoyalN1Starfighter/RicOlie.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Republic/NabooRoyalN1Starfighter/RicOlie.cs
@@ -39,8 +39,8 @@ namespace Ship
         {
             public RicOlieXWA() : base()
             {
-                (PilotInfo as PilotCardInfo25).Cost = 10;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 12;
+                (PilotInfo as PilotCardInfo25).Cost = 11;
+                (PilotInfo as PilotCardInfo25).LoadoutValue = 16;
                 (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
                 (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
                 {
