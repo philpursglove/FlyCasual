ab5011f6a01c04a05d480959c7687a93f98c9dcf
Phil Pursglove <philpursglove@users.noreply.github.com>
2025-12-09 20:57:21 +0000

    The Child

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/TheChild.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/TheChild.cs
@@ -1,6 +1,8 @@
-﻿using Ship;
+﻿using Content;
+using Ship;
 using SubPhases;
 using System;
+using System.Collections.Generic;
 using Tokens;
 using Upgrade;
 
@@ -17,8 +19,18 @@ namespace UpgradesList.SecondEdition
                 cost: 7,
                 abilityType: typeof(Abilities.SecondEdition.TheChildAbility),
                 restriction: new FactionRestriction(Faction.Imperial, Faction.Rebel, Faction.Scum),
-                addForce: 2
+                addForce: 2,
+                legalityInfo: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
             );
+        }
+    }
+
+    public class TheChildXWA : TheChild
+    {
+        public TheChildXWA() : base()
+        {
+            UpgradeInfo.Cost = 9;
+            UpgradeInfo.LegalityInfo = new List<Legality> { Legality.XWA };
         }
     }
 }
