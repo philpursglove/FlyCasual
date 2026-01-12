d4f1b5647335b9550ba547ab6e3ebe6787df1b49
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-01-12 22:10:31 +0000

    Relay System WIP

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Sensor/RelaySystem.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Sensor/RelaySystem.cs
@@ -1,5 +1,7 @@
 using Ship;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using Tokens;
 using Upgrade;
 
@@ -32,6 +34,73 @@ namespace Abilities.SecondEdition
 
         private void CheckPassOnTargetLockAbility()
         {
+            // Check for other friendly ship at range 0-1
+            List<GenericShip> friendlyShipsAtRange0To1 = new List<GenericShip>();
+
+            foreach (KeyValuePair<string, GenericShip> friendlyShip in HostShip.Owner.Ships)
+            {
+                if (friendlyShip.Value != HostShip)
+                {
+                    BoardTools.DistanceInfo distanceInfo = new BoardTools.DistanceInfo(HostShip, friendlyShip.Value);
+                    if (distanceInfo.Range < 2)
+                    {
+                        friendlyShipsAtRange0To1.Add(friendlyShip.Value);
+                    }
+                }
+            }
+
+            if (HostShip.Tokens.HasToken<BlueTargetLockToken>() && friendlyShipsAtRange0To1.Any())
+            {
+                BlueTargetLockToken existingToken = (BlueTargetLockToken)HostShip.Tokens.GetToken(typeof(BlueTargetLockToken));
+                ITargetLockable lockedObject = existingToken.OtherTargetLockTokenOwner;
+                if (lockedObject != Combat.Defender) return;
+                AskToUseAbility("Relay System",
+                    NeverUseByDefault,
+                    descriptionLong: $"You may pass your target lock to a friendly ship at range 0-1",
+                    useAbility: delegate { PassTargetLock(lockedObject, friendlyShipsAtRange0To1); });
+            }
+        }
+
+        private void PassTargetLock(ITargetLockable lockedObject, List<GenericShip> potentialRecipients)
+        {
+            // If only one possible recipient, skip the decision
+            if (potentialRecipients.Count == 1)
+            {
+                TransferTargetLock(lockedObject, potentialRecipients.First());
+            }
+            else
+            {
+                SelectTargetForAbility(
+                    TransferTargetLock,
+                    potentialRecipients,
+                    GetAiPriorityForTargetLockTransfer,
+                    description: "Select a ship to transfer the target lock to",
+                    showSkipButton: true
+                );
+            }
+        }
+
+        private int GetAiPriorityForTargetLockTransfer(GenericShip ship)
+        {
+            int priority = 0;
+            // Prefer ships that are attacking
+            if (ship.IsAttacking) priority += 100;
+            // Prefer ships that don't already have a target lock on the same target
+            if (ship.Tokens.HasToken<BlueTargetLockToken>())
+            {
+                BlueTargetLockToken existingToken = (BlueTargetLockToken)ship.Tokens.GetToken(typeof(BlueTargetLockToken));
+                if (existingToken.OtherTargetLockTokenOwner == Combat.Defender) priority -= 50;
+            }
+            // Prefer ships with lower pilot skill (assuming they benefit more from the target lock)
+            priority += (10 - ship.PilotInfo.Initiative);
+            return priority;
+        }
+
+        private void TransferTargetLock(ITargetLockable lockedObject, GenericShip recipient)
+        {
+            BlueTargetLockToken existingToken = (BlueTargetLockToken)HostShip.Tokens.GetToken(typeof(BlueTargetLockToken));
+            HostShip.RemoveToken(existingToken);
+            ActionsHolder.AcquireTargetLock(recipient, lockedObject, delegate { }, delegate { }, false);
         }
 
         public override void DeactivateAbility()
