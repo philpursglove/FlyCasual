e4ead7430773520055b6358737d91d259d140403
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-03-02 22:44:32 +0000

    Fill in Silent Hunter by copying in from Mando Optics

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/SilentHunter.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/SilentHunter.cs
@@ -1,3 +1,10 @@
+using Arcs;
+using BoardTools;
+using Ship;
+using SubPhases;
+using System.Collections.Generic;
+using System.Linq;
+using Tokens;
 using Upgrade;
 
 namespace UpgradesList.SecondEdition
@@ -21,11 +28,63 @@ namespace Abilities.SecondEdition
     {
         public override void ActivateAbility()
         {
-            // Ability implementation goes here
+            HostShip.OnDecloak += CheckAbility;
         }
+
+        private void CheckAbility()
+        {
+            // Are there any enemy ships in bullseye
+            // Offer a target lock on one of them
+            var enemyShips = HostShip.SectorsInfo.GetEnemiesInAllSectors();
+            var shipsInBullseye = enemyShips.TryGetValue(ArcFacing.Bullseye, out var bullseyeShips) ? bullseyeShips : new List<GenericShip>();
+            if (shipsInBullseye.Any())
+            {
+                SelectTargetForAbility(
+                    GrantFreeTargetLock,
+                    FilterAbilityTargets,
+                    GetAiAbilityPriority,
+                    HostShip.Owner.PlayerNo,
+                    HostUpgrade.State.Name,
+                    "You may spend 1 Charge to acquire a lock on an object in your front arc",
+                    HostUpgrade
+                );
+            }
+        }
+
+        private bool FilterAbilityTargets(GenericShip ship)
+        {
+            return HostShip.SectorsInfo.IsShipInSector(ship, ArcType.Bullseye);
+        }
+
+        private void GrantFreeTargetLock()
+        {
+            if (TargetShip != null)
+            {
+                ActionsHolder.AcquireTargetLock(HostShip, TargetShip, SelectShipSubPhase.FinishSelection, SelectShipSubPhase.FinishSelection);
+            }
+            else
+            {
+                SelectShipSubPhase.FinishSelection();
+            }
+        }
+
+        private int GetAiAbilityPriority(GenericShip ship)
+        {
+            int priority = 0;
+
+            if (!HostShip.Tokens.HasToken(typeof(BlueTargetLockToken))) priority += 50;
+            ShotInfo shotInfo = new ShotInfo(HostShip, ship, ship.PrimaryWeapons);
+            if (shotInfo.IsShotAvailable) priority += 40;
+
+            priority += ship.PilotInfo.Cost;
+
+            return priority;
+        }
+
         public override void DeactivateAbility()
         {
             // Ability deactivation logic goes here
+            HostShip.OnDecloak -= CheckAbility;
         }
     }
 }
