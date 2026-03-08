211f7a3f3b7cc1d57d0c2bd46c83e8f0455053f5
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-03-08 16:58:27 +0000

    Fix typo

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/AlphaClassStarWing/LieutenantKarsabi.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Empire/AlphaClassStarWing/LieutenantKarsabi.cs
@@ -98,7 +98,7 @@ namespace Abilities.SecondEdition
 
         private void UseAbility(object sender, System.EventArgs e)
         {
-            Messages.ShowInfo(HostShip.PilotInfo.PilotName + " recieved Stress token instead of Disarm Token");
+            Messages.ShowInfo($"{HostShip.PilotInfo.PilotName} received Stress token instead of Disarm Token");
 
             HostShip.Tokens.RemoveToken(
                 typeof(WeaponsDisabledToken),
