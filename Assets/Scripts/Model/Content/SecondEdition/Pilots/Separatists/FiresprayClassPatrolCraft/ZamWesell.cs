fc3cddb2352fadf92b89032afd5e1c77301d9058
Jeremy S <baledin@gmail.com>
2026-03-29 22:18:38 -0700

    Moved deactivate ability to the top of the class for consistency with other projects

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Separatists/FiresprayClassPatrolCraft/ZamWesell.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Separatists/FiresprayClassPatrolCraft/ZamWesell.cs
@@ -91,6 +91,19 @@ namespace Abilities.SecondEdition
             HostShip.OnSystemsPhaseStart += RemoveOwnConditions;
         }
 
+        public override void DeactivateAbility()
+        {
+            HostShip.OnSetupPlaced -= LoseChargesOnSetup;
+
+            HostShip.OnCheckSystemsAbilityActivation -= CheckAbility;
+            HostShip.OnSystemsAbilityActivation -= RegisterAbility;
+
+            HostShip.OnAttackFinishAsDefender -= CheckAttackFinishCondition;
+            Phases.Events.OnCombatPhaseEnd_Triggers -= CheckCombatFinishCondition;
+
+            HostShip.OnSystemsPhaseStart -= RemoveOwnConditions;
+        }
+
         protected virtual void LoseChargesOnSetup(GenericShip ship)
         {
             Messages.ShowInfo($"{HostShip.PilotInfo.PilotName}: 2 Charges are lost during Setup");
@@ -432,19 +445,6 @@ namespace Abilities.SecondEdition
             AssignedCondition = null;
 
             IsAbilityUsed = false;
-        }
-
-        public override void DeactivateAbility()
-        {
-            HostShip.OnSetupPlaced -= LoseChargesOnSetup;
-
-            HostShip.OnCheckSystemsAbilityActivation -= CheckAbility;
-            HostShip.OnSystemsAbilityActivation -= RegisterAbility;
-
-            HostShip.OnAttackFinishAsDefender -= CheckAttackFinishCondition;
-            Phases.Events.OnCombatPhaseEnd_Triggers -= CheckCombatFinishCondition;
-
-            HostShip.OnSystemsPhaseStart -= RemoveOwnConditions;
         }
     }
 }
