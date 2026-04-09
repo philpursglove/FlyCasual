9138de59184b684ed6b60d4f74e2bd49b0252c48
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-04-09 14:18:54 +0100

    Skeleton captive

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/Captive.cs
@@ -0,0 +1,49 @@
+﻿using Content;
+using Ship;
+using System.Collections.Generic;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class Captive : GenericUpgrade
+    {
+        public Captive() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Captive",
+                UpgradeType.Crew,
+                cost: 1, //TODO Fix cost
+                isLimited: true,
+                restriction: new FactionRestriction(Faction.Imperial, Faction.FirstOrder, Faction.Separatists),
+                abilityType: typeof(Abilities.SecondEdition.CaptiveCrewAbility),
+                legalityInfo: new List<Legality> { Legality.XWA }
+            );
+            IsHidden = false;
+
+            //TODO ImageUrl
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class CaptiveCrewAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnDefenceStartAsDefender += UseAbility();
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnDefenceStartAsDefender -= UseAbility();
+        }
+
+        private GenericShip.EventHandler UseAbility()
+        {
+            throw new System.NotImplementedException();
+        }
+
+
+    }
+}
