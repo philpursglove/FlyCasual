62f270ca5e35e5349ca2be8f53cdfdf909d4b6ff
Jeremy S. <baledin@gmail.com>
2026-04-09 14:40:22 -0700

    Unhinged Astromech Initial implementation

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Astromech/UnhingedAstromech.cs
@@ -0,0 +1,58 @@
+﻿using Content;
+using Movement;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class UnhingedAstromech : GenericUpgrade
+    {
+        public UnhingedAstromech() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Unhinged Astromech",
+                UpgradeType.Astromech,
+                cost: 1, // TODO: Update cost
+                abilityType: typeof(Abilities.SecondEdition.UnhingedAstromechAbility),
+                legalityInfo: new() { Legality.XWA }
+            );
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    //Reduce the difficulty of your speed 3 basic maneuvers.
+    public class UnhingedAstromechAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGetManeuvers += ReduceSpeedThreeDifficulty;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnGetManeuvers -= ReduceSpeedThreeDifficulty;
+        }
+
+        private void ReduceSpeedThreeDifficulty(Dictionary<string, MovementComplexity> maneuvers)
+        {
+            //"1.L.B", MovementComplexity.Easy
+            string[] basicManuevers = new string[] { "S", "B", "T" };
+
+            Dictionary<string, MovementComplexity> speedThreeMoves = maneuvers.Where(m => m.Key.Contains("3")).ToDictionary(m => m.Key, m => m.Value);
+
+            foreach (KeyValuePair<string, MovementComplexity> movement in speedThreeMoves)
+            {
+                string[] movementParts = movement.Key.Split('.');
+
+                if (basicManuevers.Contains(movementParts[2]))
+                {
+                    maneuvers[movement.Key] = GenericMovement.ReduceComplexity(movement.Value);
+                }
+            }
+        }
+    }
+}
