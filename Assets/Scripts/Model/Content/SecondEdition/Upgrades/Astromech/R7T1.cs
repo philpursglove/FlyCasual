19986dcf8716f1b3a5b584a66ff18a47caac386b
Jeremy S. <baledin@gmail.com>
2026-04-08 21:43:00 -0700

    R7-T1 initial implementation

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Astromech/R7T1.cs
@@ -0,0 +1,25 @@
+﻿using Actions;
+using ActionsList;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class R7T1 : GenericUpgrade
+    {
+        public R7T1() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "R7-T1",
+                UpgradeType.Astromech,
+                cost: 1, // TODO: Update cost
+                isLimited: true,
+                addActionLink: new LinkedActionInfo(typeof(TargetLockAction), typeof(BoostAction)),
+                restrictions: new UpgradeCardRestrictions(
+                    new ActionBarRestriction(typeof(TargetLockAction), ActionColor.White),
+                    new BaseSizeRestriction(Ship.BaseSize.Small),
+                    new FactionRestriction(Faction.Resistance, Faction.Rebel)
+                )
+            );
+        }
+    }
+}
