eebf31267b18ceb6c9634beb526458a0bd938b95
Jeremy S. <baledin@gmail.com>
2026-04-10 18:36:51 -0700

    Set Abilities to protected and virtual as appropriate; cleanup

--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Separatists/NantexClassStarfighter/Chertek.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Separatists/NantexClassStarfighter/Chertek.cs
@@ -3,50 +3,47 @@ using Ship;
 using System.Collections.Generic;
 using Upgrade;
 
-namespace Ship
+namespace Ship.SecondEdition.NantexClassStarfighter
 {
-    namespace SecondEdition.NantexClassStarfighter
+    public class Chertek : NantexClassStarfighter
     {
-        public class Chertek : NantexClassStarfighter
+        public Chertek() : base()
         {
-            public Chertek() : base()
-            {
-                PilotInfo = new PilotCardInfo25
-                (
-                    "Chertek",
-                    "Opportunistic Ace",
-                    Faction.Separatists,
-                    4,
-                    4,
-                    10,
-                    isLimited: true,
-                    abilityType: typeof(Abilities.SecondEdition.ChertekAbility),
-                    abilityText: "While you perform a primary attack, if the defender is tractored, you may reroll up to 2 attack dice.",
-                    extraUpgradeIcons: new List<UpgradeType>
-                    {
-                        UpgradeType.Talent,
-                        UpgradeType.Talent
-                    },
-                    legality: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
-                );
-            }
-        }
-
-        public class ChertekXWA : Chertek
-        {
-            public ChertekXWA() : base()
-            {
-                (PilotInfo as PilotCardInfo25).Cost = 10;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 14;
-                (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
+            PilotInfo = new PilotCardInfo25
+            (
+                "Chertek",
+                "Opportunistic Ace",
+                Faction.Separatists,
+                4,
+                4,
+                10,
+                isLimited: true,
+                abilityType: typeof(Abilities.SecondEdition.ChertekAbility),
+                abilityText: "While you perform a primary attack, if the defender is tractored, you may reroll up to 2 attack dice.",
+                extraUpgradeIcons: new List<UpgradeType>
                 {
                     UpgradeType.Talent,
-                    UpgradeType.Talent,
-                    UpgradeType.Modification,
-                    UpgradeType.Modification
-                };
-                (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
-            }
+                    UpgradeType.Talent
+                },
+                legality: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
+            );
+        }
+    }
+
+    public class ChertekXWA : Chertek
+    {
+        public ChertekXWA() : base()
+        {
+            (PilotInfo as PilotCardInfo25).Cost = 10;
+            (PilotInfo as PilotCardInfo25).LoadoutValue = 14;
+            (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
+            {
+                UpgradeType.Talent,
+                UpgradeType.Talent,
+                UpgradeType.Modification,
+                UpgradeType.Modification
+            };
+            (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
         }
     }
 }
@@ -66,22 +63,21 @@ namespace Abilities.SecondEdition
             );
         }
 
-        private bool IsAvailable()
+        public override void DeactivateAbility()
+        {
+            RemoveDiceModification();
+        }
+
+        protected virtual bool IsAvailable()
         {
             return Combat.AttackStep == CombatStep.Attack
                 && Combat.ChosenWeapon.WeaponType == WeaponTypes.PrimaryWeapon
                 && Combat.Defender.IsTractored;
         }
 
-        private int GetAiPriority()
+        protected int GetAiPriority()
         {
             return 90;
         }
-
-        public override void DeactivateAbility()
-        {
-            RemoveDiceModification();
-        }
-
     }
 }
--- Assets/Scripts/Model/Content/SecondEdition/Pilots/Separatists/NantexClassStarfighter/SunFac.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Separatists/NantexClassStarfighter/SunFac.cs
@@ -2,53 +2,50 @@
 using System.Collections.Generic;
 using Upgrade;
 
-namespace Ship
+namespace Ship.SecondEdition.NantexClassStarfighter
 {
-    namespace SecondEdition.NantexClassStarfighter
+    public class SunFac : NantexClassStarfighter
     {
-        public class SunFac : NantexClassStarfighter
+        public SunFac() : base()
         {
-            public SunFac() : base()
-            {
-                PilotInfo = new PilotCardInfo25
-                (
-                    "Sun Fac",
-                    "Archduke’s Enforcer",
-                    Faction.Separatists,
-                    6,
-                    5,
-                    15,
-                    isLimited: true,
-                    abilityType: typeof(Abilities.SecondEdition.SunFacAbility),
-                    abilityText: "while you perform a primary attack, if the defender is tractored, roll 1 additional attack die.",
-                    extraUpgradeIcons: new List<UpgradeType>
-                    {
-                        UpgradeType.Talent,
-                        UpgradeType.Talent,
-                        UpgradeType.Talent,
-                        UpgradeType.Modification
-                    },
-                    legality: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
-                );
-            }
-        }
-
-        public class SunFacXWA : SunFac
-        {
-            public SunFacXWA() : base()
-            {
-                (PilotInfo as PilotCardInfo25).Cost = 13;
-                (PilotInfo as PilotCardInfo25).LoadoutValue = 17;
-                (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
+            PilotInfo = new PilotCardInfo25
+            (
+                "Sun Fac",
+                "Archduke’s Enforcer",
+                Faction.Separatists,
+                6,
+                5,
+                15,
+                isLimited: true,
+                abilityType: typeof(Abilities.SecondEdition.SunFacAbility),
+                abilityText: "while you perform a primary attack, if the defender is tractored, roll 1 additional attack die.",
+                extraUpgradeIcons: new List<UpgradeType>
                 {
                     UpgradeType.Talent,
                     UpgradeType.Talent,
                     UpgradeType.Talent,
-                    UpgradeType.Modification,
                     UpgradeType.Modification
-                };
-                (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
-            }
+                },
+                legality: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
+            );
+        }
+    }
+
+    public class SunFacXWA : SunFac
+    {
+        public SunFacXWA() : base()
+        {
+            (PilotInfo as PilotCardInfo25).Cost = 13;
+            (PilotInfo as PilotCardInfo25).LoadoutValue = 17;
+            (PilotInfo as PilotCardInfo25).ExtraUpgrades = new List<UpgradeType>
+            {
+                UpgradeType.Talent,
+                UpgradeType.Talent,
+                UpgradeType.Talent,
+                UpgradeType.Modification,
+                UpgradeType.Modification
+            };
+            (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
         }
     }
 }
@@ -67,7 +64,7 @@ namespace Abilities.SecondEdition
             HostShip.AfterGotNumberOfPrimaryWeaponAttackDice -= CheckAbility;
         }
 
-        private void CheckAbility(ref int count)
+        protected virtual void CheckAbility(ref int count)
         {
             if (Combat.Defender.IsTractored)
             {
