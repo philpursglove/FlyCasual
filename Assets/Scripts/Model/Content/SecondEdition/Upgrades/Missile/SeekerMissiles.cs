75f68afd1f7b86eb06ba9be084c680b2172cea4c
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-04-13 13:36:37 +0100

    Initial creation

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Missile/SeekerMissiles.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using Tokens;
+using Upgrade;
+
+namespace Content.SecondEdition
+{
+    public class SeekerMissiles : GenericSpecialWeapon
+    {
+        public SeekerMissiles() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Seeker Missiles",
+                types: new List<UpgradeType>()
+                {
+                    UpgradeType.Missile
+                },
+                cost: 0,
+                weaponInfo: new SpecialWeaponInfo(
+                    attackValue: 3,
+                    minRange: 2,
+                    maxRange: 3,
+                    charges: 4,
+                    requiresToken: typeof(BlueTargetLockToken)
+                ),
+                abilityType: typeof(Abilities.SecondEdition.SeekerMissilesAbility),
+                legalityInfo: new() { Legality.StandardLegal, Legality.ExtendedLegal }
+            );
+            IsHidden = true;
+        }
+    }
+
+    public class SeekerMissilesXWA : SeekerMissiles
+    {
+        public SeekerMissilesXWA() : base()
+        {
+            UpgradeInfo.Cost = 1;
+            UpgradeInfo.LegalityInfo = new() { Legality.XWA };
+            IsHidden = false;
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class SeekerMissilesAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            AddDiceModification(name: "Seeker Missiles",
+                isAvailable: IsAvailable,
+                aiPriority: GetAIPriority,
+                modificationType: DiceModificationType.Change,
+                sidesCanBeSelected: new List<DieSide>() { DieSide.Focus },
+                sideCanBeChangedTo: DieSide.Success,
+                payAbilityPostCost: PayAbilityCost,
+                count: 1);
+        }
+
+        public override void DeactivateAbility()
+        {
+            RemoveDiceModification();
+        }
+
+        private bool IsAvailable()
+        {
+            return Combat.DiceRollAttack.Focuses > 0;
+        }
+
+        private int GetAIPriority()
+        {
+            return 80;
+        }
+
+        private void PayAbilityCost()
+        {
+            for (int i = 0; i < DiceRoll.CurrentDiceRoll.DiceWereSelectedForRerollCount; i++)
+            {
+                HostUpgrade.State.SpendCharge();
+            }
+        }
+    }
+}
