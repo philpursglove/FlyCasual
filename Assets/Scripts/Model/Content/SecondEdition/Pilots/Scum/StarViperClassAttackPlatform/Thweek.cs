a557146d2c35c4931db511261692b29836949bec
Jeremy S. <baledin@gmail.com>
2026-04-16 12:09:40 -0700

    Initial implementation

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/StarViperClassAttackPlatform/Thweek.cs
@@ -0,0 +1,102 @@
+﻿using Abilities.SecondEdition;
+using Content;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using Tokens;
+
+namespace Ship.SecondEdition.StarViperClassAttackPlatform
+{
+    public class Thweek : StarViperClassAttackPlatform
+    {
+        public Thweek() : base()
+        {
+            PilotInfo = new PilotCardInfo25(
+                pilotName: "Thweek",
+                pilotTitle: "Versatile Spy",
+                faction: Faction.Scum,
+                initiative: 4,
+                cost: 25, // TODO: Update
+                loadoutValue: 50, // TODO: Update
+                isLimited: true,
+                abilityType: typeof(ThweekAbility),
+                legality: new List<Legality>() { Legality.XWA }
+            );
+
+            PilotNameCanonical = "thweek-legendsandrelics";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    // During the System Phase, you may gain a tractor token.
+    // At the start of the Engagement Phase, you may gain a deplete token to remove a tractor token.
+
+    public class ThweekAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnSystemsPhaseStart += RegisterSystemPhaseAbility;
+            Phases.Events.OnCombatPhaseStart_Triggers += RegisterEngagementPhaseAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnSystemsPhaseStart -= RegisterSystemPhaseAbility;
+            Phases.Events.OnCombatPhaseStart_Triggers -= RegisterEngagementPhaseAbility;
+        }
+
+        private void RegisterSystemPhaseAbility(GenericShip ship)
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnSystemsPhaseStart, AskGainTractorToken);
+        }
+
+        private void AskGainTractorToken(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                NeverUseByDefault,
+                GainTractorToken,
+                callback: Triggers.FinishTrigger,
+                descriptionLong: $"Would you like to gain a tractor token?",
+                imageHolder: HostShip,
+                requiredPlayer: HostShip.Owner.PlayerNo
+            );
+        }
+
+        private void GainTractorToken(object sender, EventArgs e)
+        {
+            HostShip.Tokens.AssignToken(typeof(TractorBeamToken), DecisionSubPhase.ConfirmDecision);
+        }
+
+        private void RegisterEngagementPhaseAbility()
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnCombatPhaseStart, AskGainDepleteTokenToRemoveTractor);
+        }
+
+        private void AskGainDepleteTokenToRemoveTractor(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                NeverUseByDefault,
+                GainDepleteAndRemoveTractorTokens,
+                callback: Triggers.FinishTrigger,
+                descriptionLong: $"Would you like to gain a tractor token?",
+                imageHolder: HostShip,
+                requiredPlayer: HostShip.Owner.PlayerNo
+            );
+        }
+
+        private void GainDepleteAndRemoveTractorTokens(object sender, EventArgs e)
+        {
+            HostShip.Tokens.AssignToken(typeof(DepleteToken), RemoveTractorBeamToken);
+        }
+
+        private void RemoveTractorBeamToken()
+        {
+            HostShip.Tokens.RemoveToken(typeof(TractorBeamToken), DecisionSubPhase.ConfirmDecision);
+        }
+    }
+}
