2c3188fe88e0ea50fe38bee818918c1afc343779
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-04-19 16:05:29 +0100

    Revert the Delayed Fuses change cos I'm a plonker

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Modification/DelayedFuses.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Modification/DelayedFuses.cs
@@ -13,7 +13,7 @@ namespace UpgradesList.SecondEdition
         {
             UpgradeInfo = new UpgradeCardInfo("Delayed Fuses",
                 UpgradeType.Modification,
-                cost: 2,
+                cost: 1,
                 abilityType: typeof(Abilities.SecondEdition.DelayedFusesAbility),
                 seImageNumber: 74,
                 legalityInfo: new List<Legality>() { Legality.StandardLegal, Legality.ExtendedLegal }
