4e8ba268922ecda0d5b008ea4d774a85d1496205
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-04-19 16:02:02 +0100

    Update Roiling Anger cost

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/ForcePower/RoilingAnger.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/ForcePower/RoilingAnger.cs
@@ -16,7 +16,7 @@ namespace UpgradesList.SecondEdition
             (
                 "Roiling Anger",
                 UpgradeType.ForcePower,
-                cost: 0,
+                cost: 2,
                 abilityType: typeof(Abilities.SecondEdition.RoilingAngerAbility),
                 legalityInfo: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal }
             );
