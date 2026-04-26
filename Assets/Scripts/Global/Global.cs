4eeeb78c1bc8fa08a1a4d75197ba47931706e9b5
Jeremy S. <baledin@gmail.com>
2026-04-26 08:49:55 -0700

    Updated version

--- Assets/Scripts/Global/Global.cs
+++ Assets/Scripts/Global/Global.cs
@@ -13,8 +13,8 @@ public class Global : MonoBehaviour
 
     private static bool isAlreadyInitialized;
 
-    public static string CurrentVersion = "v2.5.20251209.06";
-    public static int CurrentVersionInt = 102507006;  // literal(10) XwingVersion(25) MajorUpdate#(increments) MinorUpdate#(increments)
+    public static string CurrentVersion = "v2.5.20250426.01";
+    public static int CurrentVersionInt = 102508001;  // literal(10) XwingVersion(25) MajorUpdate#(increments) MinorUpdate#(increments)
     public static int LatestVersionInt = 0;
 
     // Used for json exports, update when vendor version changes
