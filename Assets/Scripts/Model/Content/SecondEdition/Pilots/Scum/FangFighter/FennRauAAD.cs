29b65460485ba94385b65e58d1ab272b5cca2616
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-04-27 09:29:47 +0100

    Initial commit of Fenn AaD

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/Scum/FangFighter/FennRauAAD.cs
@@ -0,0 +1,43 @@
+using Content;
+using System.Collections.Generic;
+using Upgrade;
+using UpgradesList.SecondEdition;
+
+namespace Ship.SecondEdition.FangFighter
+{
+    public class FennRauAaD : FangFighter
+    {
+        public FennRauAaD() : base()
+        {
+            PilotInfo = new PilotCardInfo25
+            (
+                "Fenn Rau",
+                "Armed and Dangerous",
+                Faction.Scum,
+                6,
+                17,
+                0,
+                isLimited: true,
+                abilityType: typeof(Abilities.SecondEdition.FennRauScumAbility),
+
+                tags: new List<Tags>
+                {
+                    Tags.Mandalorian
+                },
+                extraUpgradeIcons: new List<UpgradeType>
+                {
+                    UpgradeType.Talent,
+                    UpgradeType.Modification,
+                    UpgradeType.Modification
+                },
+                seImageNumber: 155,
+                skinName: "Zealous Recruit",
+                legality: new List<Legality> { Legality.XWA },
+                isStandardLayout: true
+            );
+            MustHaveUpgrades.Add(typeof(Fearless));
+            MustHaveUpgrades.Add(typeof(BeskarReinforcedPlating));
+            MustHaveUpgrades.Add(typeof(AdaptivePowerSystems));
+        }
+    }
+}
