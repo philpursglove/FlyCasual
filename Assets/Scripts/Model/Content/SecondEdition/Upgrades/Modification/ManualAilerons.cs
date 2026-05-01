170884d347421b47248ef23ba2d7cb685bcae9bc
Jeremy S. <baledin@gmail.com>
2026-05-01 08:52:07 -0700

    Updated to use new DecloakSubphase template updates, and register Ask before actually decloaking but before moving.

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Modification/ManualAilerons.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Modification/ManualAilerons.cs
@@ -1,5 +1,7 @@
 using BoardTools;
 using Movement;
+using SubPhases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Upgrade;
@@ -16,52 +18,85 @@ namespace UpgradesList.SecondEdition
                 abilityType: typeof(Abilities.SecondEdition.ManualAileronsAbility),
                 charges: 2
             );
+
+            IsHidden = true;
         }
     }
 }
 
 namespace Abilities.SecondEdition
 {
+    // When you decloack, you may spend 1 charge to use the 2 bank template instead of the straight 2 template
+
     public class ManualAileronsAbility : GenericAbility
     {
         public override void ActivateAbility()
         {
-            HostShip.OnGetAvailableDecloakTemplates += CheckAbility;
+            HostShip.OnBeforeDecloak += RegisterAbility;
         }
 
         public override void DeactivateAbility()
         {
-            HostShip.OnGetAvailableDecloakTemplates -= CheckAbility;
+            HostShip.OnBeforeDecloak -= RegisterAbility;
         }
 
-        private void CheckAbility(List<ManeuverTemplate> availableTemplates)
+        private void RegisterAbility()
+        {
+            RegisterAbilityTrigger(TriggerTypes.OnBeforeDecloak, ActivateAbility);
+        }
+
+        private void ActivateAbility(object sender, EventArgs e)
         {
             if (HostUpgrade.State.Charges > 0)
             {
-                // Ask the player if they want to spend a charge to use a 2-speed template instead of the 1-speed template to barrel roll or boost
-                AskToUseAbility("Manual Ailerons",
+                AskToUseAbility(HostUpgrade.UpgradeInfo.Name,
                     NeverUseByDefault,
-                    descriptionLong:
-                    "You may spend 1 charge to use a 2-speed template instead of the 1-speed template to barrel roll or boost",
-                    useAbility: delegate { UseManualAilerons(availableTemplates); }
+                    descriptionLong: "You may spend 1 charge to use a 2-speed bank template instead of the 2-speed straight template.",
+                    useAbility: UseManualAilerons
+                    , callback: Triggers.FinishTrigger
                 );
+            }
+            else
+            {
+                Triggers.FinishTrigger();
             }
         }
 
-        private void UseManualAilerons(List<ManeuverTemplate> availableTemplates)
+        private void UseManualAilerons(object sender, EventArgs e)
         {
-            HostUpgrade.State.SpendCharge();
+            HostShip.OnGetAvailableDecloakBarrelRollTemplates += GetBarrelRollDecloackTemplates;
+            HostShip.OnGetAvailableDecloakBoostTemplates += GetBoostDecloakTemplates;
+            HostShip.OnDecloak += SpendCharge;
 
+            DecisionSubPhase.ConfirmDecision();
+        }
+
+        private void GetBarrelRollDecloackTemplates(List<ManeuverTemplate> availableTemplates)
+        {
+            HostShip.OnGetAvailableDecloakBarrelRollTemplates -= GetBarrelRollDecloackTemplates;
+            GetDecloakTemplates(availableTemplates);
+        }
+
+        private void GetBoostDecloakTemplates(List<ManeuverTemplate> availableTemplates)
+        {
+            HostShip.OnGetAvailableDecloakBoostTemplates -= GetBoostDecloakTemplates;
+            GetDecloakTemplates(availableTemplates);
+        }
+
+        private void GetDecloakTemplates(List<ManeuverTemplate> availableTemplates)
+        {
             if (availableTemplates.Any(n => n.Name == "Straight 2"))
             {
                 availableTemplates.RemoveAll(n => n.Name == "Straight 2");
                 availableTemplates.Add(new ManeuverTemplate(ManeuverBearing.Bank, ManeuverDirection.Left, ManeuverSpeed.Speed2));
                 availableTemplates.Add(new ManeuverTemplate(ManeuverBearing.Bank, ManeuverDirection.Right, ManeuverSpeed.Speed2));
-                availableTemplates.Add(new ManeuverTemplate(ManeuverBearing.SideslipBank, ManeuverDirection.Left, ManeuverSpeed.Speed2));
-                availableTemplates.Add(new ManeuverTemplate(ManeuverBearing.SideslipBank, ManeuverDirection.Right, ManeuverSpeed.Speed2));
             }
+        }
 
-            Triggers.FinishTrigger();
+        private void SpendCharge()
+        {
+            HostUpgrade.State.SpendCharge();
+            HostShip.OnDecloak -= SpendCharge;
         }
     }
 }
