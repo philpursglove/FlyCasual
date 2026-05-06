66febba67dbf07fab62bf1a167bc83523ec85626
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-05-06 12:51:59 +0100

    KinesoSwitch WIP

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Illicit/KinesoSwitch.cs
@@ -0,0 +1,41 @@
+﻿using ActionsList;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class KinesoSwitch : GenericUpgrade
+    {
+        public KinesoSwitch() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Kineso-Switch",
+                UpgradeType.Illicit,
+                cost: 0,
+                charges: 2,
+                abilityType: typeof(Abilities.SecondEdition.KinesoSwitchAbility)
+            );
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class KinesoSwitchAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += RegisterKinesoSwitch;
+        }
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= RegisterKinesoSwitch;
+        }
+        private void RegisterKinesoSwitch(GenericAction action)
+        {
+            if (HostUpgrade.State.Charges > 0 && !HostShip.IsBumped)
+            {
+
+            }
+        }
+    }
+}
