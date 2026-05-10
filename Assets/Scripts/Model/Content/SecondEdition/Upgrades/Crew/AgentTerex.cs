b814e97ad9c267af0108f44b37440d96d6ce948c
Ergiuwh <282158786+Ergiuwh@users.noreply.github.com>
2026-05-10 20:20:48 +1000

    Add isActionAvailable to AgentTerexCyborgAction
    Add isActionAvailable to AgentTerexCyborgAction
    Fix typo in subphase name

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/AgentTerex.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Crew/AgentTerex.cs
@@ -216,7 +216,7 @@ namespace Abilities.SecondEdition
         private static void StartSelectionSubphase(GenericToken token)
         {
             AgentTerexCyborgActionSubPhase subPhase = Phases.StartTemporarySubPhaseNew<AgentTerexCyborgActionSubPhase>(
-                "Agen Terex Target Selection",
+                "Agent Terex Target Selection",
                 Phases.CurrentSubPhase.CallBack
             );
 
@@ -236,6 +236,11 @@ namespace Abilities.SecondEdition
             }
 
             return result;
+        }
+        public override bool IsActionAvailable()
+        {
+            return Selection.ThisShip.Tokens.HasToken<CalculateToken>()
+                || Selection.ThisShip.Tokens.HasToken<JamToken>();
         }
     }
 }
