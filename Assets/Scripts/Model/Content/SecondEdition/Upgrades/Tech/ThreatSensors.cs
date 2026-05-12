461fe4869be093105cfaa77e56f45ed33f3da476
Jeremy S. <baledin@gmail.com>
2026-05-11 18:36:51 -0700

    Initial implementation Scorch EOD and Threat Sensors.

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Tech/ThreatSensors.cs
@@ -0,0 +1,68 @@
+﻿using Abilities.SecondEdition;
+using System;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class ThreatSensors : GenericUpgrade
+    {
+        public ThreatSensors() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "Threat Sensors",
+                UpgradeType.Tech,
+                cost: 0,
+                isLimited: true,
+                abilityType: typeof(ThreatSensorsAbility)
+            );
+
+            IsHidden = true;
+
+            ImageUrl = "https://infinitearenas.com/xw2xwa/images/pilots/scorch-evacuationofdqar.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class ThreatSensorsAbility : GenericAbility
+    {
+        // While you defend, if you are not stressed, you may reroll 1 focus result.
+
+        public override void ActivateAbility()
+        {
+            AddDiceModification(
+                name: HostUpgrade.UpgradeInfo.Name,
+                isAvailable: ModificationIsAvailable,
+                aiPriority: GetAiPriority,
+                modificationType: DiceModificationType.Reroll,
+                count: 1,
+                sidesCanBeSelected: new() { DieSide.Focus }
+            );
+        }
+
+        public override void DeactivateAbility()
+        {
+            throw new NotImplementedException();
+        }
+
+        private int GetAiPriority()
+        {
+            int priority = 100;
+
+            if (HostShip.Tokens.HasGreenTokens)
+            {
+                priority -= 50;
+            }
+
+            return priority;
+        }
+
+        private bool ModificationIsAvailable()
+        {
+            return Combat.Defender == HostShip
+                && !HostShip.IsStressed
+                && Combat.DiceRollDefence.FocusesNotRerolled > 0;
+        }
+    }
+}
