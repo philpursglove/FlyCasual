eaa5d25bb1c65025a32868ecfa5f6e4ff550ec01
Jeremy S. <baledin@gmail.com>
2026-05-13 12:44:36 -0700

    Initial implementation (untested)

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/FO/TIESfFighter/Theta4EoD.cs
@@ -0,0 +1,123 @@
+﻿using Abilities.SecondEdition;
+using ActionsList;
+using Content;
+using Ship;
+using System.Collections.Generic;
+using Upgrade;
+using UpgradesList.SecondEdition;
+
+namespace Ship.SecondEdition.TIESfFighter
+{
+    public class Theta4EoD : TIESfFighter
+    {
+        public Theta4EoD() : base()
+        {
+            PilotInfo = new PilotCardInfo25(
+                pilotName: "Theta 4",
+                pilotTitle: "Evacuation of D'Qar",
+                faction: Faction.FirstOrder,
+                initiative: 4,
+                cost: 11,
+                loadoutValue: 0,
+                isLimited: true,
+                limited: 1,
+                charges: 1,
+                regensCharges: 1,
+                abilityType: typeof(Theta4EoDAbility),
+                extraUpgradeIcons: new()
+                {
+                    UpgradeType.Talent,
+                    UpgradeType.Missile,
+                    UpgradeType.Missile,
+                    UpgradeType.Tech
+                },
+                tags: new()
+                {
+                    Tags.Tie
+                },
+                isStandardLayout: true,
+                legality: new List<Legality>() { Legality.XWA }
+            );
+
+            PilotNameCanonical = "theta4-evacuationofdqar";
+
+            //MustHaveUpgrades.Add(typeof(Determination));
+            MustHaveUpgrades.Add(typeof(BarrageRockets));
+            MustHaveUpgrades.Add(typeof(PatternAnalyzer));
+
+            ShipAbilities.Add(new HeavyWeaponTurretEoD());
+
+            ShipInfo.ActionIcons.LinkedActions.Clear();
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class Theta4EoDAbility : GenericAbility
+    {
+        // While an enemy ship in your turret arc performs an attack, you may spend 1 charge. If you do, spend 1 charge from your missile upgrade to choose two attack dice.
+        // The attacker must reroll those dice.
+
+        public override void ActivateAbility()
+        {
+            GenericShip.OnGenerateDiceModificationsOppositeGlobal += RegisterAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.OnGenerateDiceModificationsOppositeGlobal -= RegisterAbility;
+        }
+
+        private void RegisterAbility(GenericShip ship)
+        {
+            ship.AddAvailableDiceModification(
+                new Theta4DiceModification()
+                {
+                    HostShip = HostShip
+                },
+                HostShip
+            );
+        }
+
+        private class Theta4DiceModification : GenericAction
+        {
+            public Theta4DiceModification()
+            {
+                Name = "Theta 4";
+
+                IsReroll = true;
+                DiceModificationTiming = DiceModificationTimingType.Opposite;
+            }
+
+            public override bool IsDiceModificationAvailable()
+            {
+                return HostShip.State.Charges > 0
+                    && HostShip.UpgradeBar.GetInstalledUpgrade(UpgradeType.Missile).State.Charges > 0
+                    && Combat.AttackStep == CombatStep.Attack
+                    && Tools.IsAnotherTeam(HostShip, Combat.Attacker)
+                    && HostShip.SectorsInfo.HasShipInTurretArc(Combat.Attacker);
+            }
+
+            public override int GetDiceModificationPriority()
+            {
+                return 50; // We may want to customize this
+            }
+
+            public override void ActionEffect(System.Action callBack)
+            {
+                HostShip.SpendCharge();
+                HostShip.UpgradeBar.GetInstalledUpgrade(UpgradeType.Missile).State.SpendCharge();
+
+                DiceRerollManager diceRerollManager = new DiceRerollManager
+                {
+                    NumberOfDiceCanBeRerolled = 2,
+                    IsOpposite = true,
+                    CallBack = callBack
+                };
+
+                diceRerollManager.Start();
+            }
+        }
+    }
+}
