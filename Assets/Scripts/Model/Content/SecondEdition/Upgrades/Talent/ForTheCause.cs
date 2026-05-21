1a2b6f3c552b7ab5ef4bfde37cf6fa06ad7d8187
Ergiuwh <282158786+Ergiuwh@users.noreply.github.com>
2026-05-21 12:09:26 +1000

    Initial draft of for the cause and JaycrisTubbsEoD
    Known error when skipping for the cause after selecting it.

---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/ForTheCause.cs
@@ -0,0 +1,158 @@
+using Content;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Upgrade;
+using UpgradesList.SecondEdition;
+using Ship;
+using SubPhases;
+using Tokens;
+using ActionsList;
+using Abilities;
+
+namespace UpgradesList.SecondEdition
+{
+    public class ForTheCause : GenericUpgrade
+    {
+        public ForTheCause() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "For The Cause",
+                UpgradeType.Talent,
+                cost: 0,
+                abilityType: typeof(Abilities.SecondEdition.ForTheCauseAbility),
+                legalityInfo: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal, Legality.XWA }
+            );
+            IsHidden = true;
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    // While you defend or perform an attack, you may spend 1 non-blank result to choose a friendly strained or depleted ship at range 1-2.
+    // That ship may remove 1 strain or deplete token.
+    public class ForTheCauseAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnGenerateDiceModifications += ForTheCauseEffect;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnGenerateDiceModifications -= ForTheCauseEffect;
+        }
+
+        private void ForTheCauseEffect(GenericShip host)
+        {
+            GenericAction newAction = new ActionsList.SecondEdition.ForTheCauseEffect()
+            {
+                HostShip = host,
+                ImageUrl = HostUpgrade.ImageUrl,
+                DoDiceModification = UseAbilityAction
+            };
+            host.AddAvailableDiceModificationOwn(newAction);
+        }
+
+        private void UseAbilityAction(Action action)
+        {
+            SelectTargetForAbility(
+                AskRemoveStrainOrDeplete,
+                AnotherFriendlyShipInRange,
+                ShipTargetAiPriority,
+                HostShip.Owner.PlayerNo,
+                HostName,
+                "Choose a ship, that ship will remove one strain or deplete token.",
+                HostUpgrade,
+                true
+            );
+        }
+
+        private bool AnotherFriendlyShipInRange(GenericShip ship)
+        {
+            return FilterByTargetType(ship, new List<TargetTypes>() { TargetTypes.AnyFriendly })
+                && FilterTargetsByRange(ship, 1, 2)
+                && (ship.Tokens.HasToken<StrainToken>() || ship.Tokens.HasToken<DepleteToken>());
+        }
+
+        private void AskRemoveStrainOrDeplete()
+        {
+            SubPhases.SelectShipSubPhase.FinishSelectionNoCallback();
+            
+            if (TargetShip.Tokens.HasToken<DepleteToken>() && TargetShip.Tokens.HasToken<StrainToken>())
+            {
+                AskToUseAbility(
+                    "Choose token to remove",
+                    AlwaysUseByDefault, // Ai Currently always removes a deplete.
+                    useAbility: delegate { RemoveDeplete(); },
+                    dontUseAbility: delegate { RemoveStrain(); },
+                    descriptionLong: "Do you want to remove Deplete token (otherwise Strain token will be removed)",
+                    showSkipButton: false,
+                    requiredPlayer: HostShip.Owner.PlayerNo
+                );
+            }
+            else if (TargetShip.Tokens.HasToken<DepleteToken>())
+            {
+                RemoveDeplete();
+            }
+            else
+            {
+                RemoveStrain();
+            }
+        }
+
+        private void RemoveStrain()
+        {
+            TargetShip.Tokens.SpendToken(typeof(StrainToken),()=>{});
+        }
+
+        private void RemoveDeplete()
+        {
+            TargetShip.Tokens.SpendToken(typeof(DepleteToken),()=>{});
+        }
+
+        private int ShipTargetAiPriority(GenericShip ship)
+        {
+            return 1; //todo!
+        }
+    }
+}
+
+namespace ActionsList.SecondEdition
+{
+    public class ForTheCauseEffect : GenericAction
+    {
+        public ForTheCauseEffect() : base()
+        {
+            Name = DiceModificationName =  "For The Cause";
+        }
+
+        public override void ActionEffect(Action callBack)
+        {
+            if (Combat.DiceRollAttack.HasResult(DieSide.Focus)) {
+                Combat.DiceRollAttack.RemoveType(DieSide.Focus);
+            } 
+            else
+            if (Combat.DiceRollAttack.HasResult(DieSide.Success)) {
+                Combat.DiceRollAttack.RemoveType(DieSide.Success);
+            }
+            else
+            {
+                Combat.DiceRollAttack.RemoveType(DieSide.Crit);
+            }
+            base.ActionEffect(callBack);
+        }
+
+        public override bool IsDiceModificationAvailable()
+        {
+            return Combat.AttackStep == CombatStep.Attack
+                && (Combat.DiceRollAttack.HasResult(DieSide.Focus) || Combat.DiceRollAttack.HasResult(DieSide.Crit) || Combat.DiceRollAttack.HasResult(DieSide.Success));
+        }
+
+        public override int GetDiceModificationPriority()
+        {
+            return 0;
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Talent/JaycrisTubbsEoD.cs
@@ -0,0 +1,171 @@
+using Abilities.SecondEdition;
+using ActionsList;
+using Content;
+using Movement;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using Tokens;
+using Upgrade;
+using UpgradesList.SecondEdition;
+using Actions;
+
+namespace Ship.SecondEdition.T70XWing
+{
+    public class JaycrisTubbsEoD : T70XWing
+    {
+        public JaycrisTubbsEoD() : base()
+        {
+            PilotInfo = new PilotCardInfo25
+            (
+                "Jaycris Tubbs",
+                "Evacuation of D'Qar",
+                Faction.Resistance,
+                1,
+                4, // todo!
+                0,
+                isLimited: true,
+                abilityType: typeof(Abilities.SecondEdition.JaycrisTubbsAbility),
+                extraUpgradeIcons: new List<UpgradeType>
+                {
+                    UpgradeType.Talent,
+                    UpgradeType.Astromech,
+                },
+                tags: new List<Tags>
+                {
+                    Tags.XWing
+                },
+                legality: new List<Legality> { Legality.StandardLegal, Legality.ExtendedLegal },
+                isStandardLayout: true
+            );
+            ShipAbilities.Add(new Abilities.SecondEdition.AdaptiveSFoils());
+
+            MustHaveUpgrades.Add(typeof(ForTheCause));
+            MustHaveUpgrades.Add(typeof(ROAstromech));
+
+            ShipInfo.ActionIcons.AddLinkedAction(new LinkedActionInfo(typeof(FocusAction), typeof(BarrelRollAction)));
+            ShipInfo.ActionIcons.AddActions(new ActionInfo(typeof(BarrelRollAction)));
+            ShipInfo.ActionIcons.AddLinkedAction(new LinkedActionInfo(typeof(BoostAction), typeof(FocusAction)));
+
+            PilotNameCanonical = "jaycristubbs-evacuationofdqar";
+        }
+    }
+
+    public class JaycrisTubbsEoDXWA : JaycrisTubbsEoD
+    {
+        public JaycrisTubbsEoDXWA() : base()
+        {
+            (PilotInfo as PilotCardInfo25).Cost = 10;
+            (PilotInfo as PilotCardInfo25).LegalityInfo = new List<Legality> { Legality.XWA };
+        }
+    }
+}
+
+namespace UpgradesList.SecondEdition
+{
+    public class ROAstromech : GenericUpgrade
+    {
+        public ROAstromech() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                "RO Astromech",
+                UpgradeType.Astromech,
+                abilityType: typeof(Abilities.SecondEdition.ROAstromechAbility),
+                legalityInfo: new() { Legality.StandardLegal, Legality.ExtendedLegal, Legality.XWA }
+            );
+            IsHidden = true;
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    // After you perform a Barrel Roll action, gain a deplete token.
+    // While you perform an attack, before rolling attack dice, if the defender is in your bullseye, you may remove 1 deplete token.
+    public class AdaptiveSFoils : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += CheckActionAbility;
+            HostShip.OnAttackStartAsAttacker += AdaptiveSFoilsAttackAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= CheckActionAbility;
+            HostShip.OnAttackStartAsAttacker -= AdaptiveSFoilsAttackAbility;
+        }
+
+        private void CheckActionAbility(GenericAction action)
+        {
+            if (action is BarrelRollAction)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnMovementFinish, GainDeplete);
+            }
+        }
+
+        private void GainDeplete(object sender, EventArgs e)
+        {
+            HostShip.Tokens.AssignToken(new DepleteToken(HostShip),Triggers.FinishTrigger);
+        }
+
+        private void AdaptiveSFoilsAttackAbility()
+        {
+            if (!HostShip.SectorsInfo.IsShipInSector(Combat.Defender, Arcs.ArcType.Bullseye))
+            {
+                HostShip.Tokens.RemoveToken(typeof(DepleteToken),()=>{});
+            }
+        }
+    }
+
+    // After you reveal a basic maneuver, you may reduce its difficulty.
+    // If you do, after the Check Difficulty step, gain 1 strain token.
+    public class ROAstromechAbility : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnManeuverIsRevealed += CheckManeuverForAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnManeuverIsRevealed -= CheckManeuverForAbility;
+        }
+
+        private void CheckManeuverForAbility(GenericShip ship)
+        {
+            if (HostShip.AssignedManeuver.IsBasicManeuver
+                && (HostShip.AssignedManeuver.ColorComplexity == Movement.MovementComplexity.Normal || HostShip.AssignedManeuver.ColorComplexity == Movement.MovementComplexity.Complex)
+            )
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnManeuverIsRevealed, RegisterROAstromechAbility);
+            }
+        }
+
+        private void RegisterROAstromechAbility(object sender, EventArgs e)
+        {
+            AskToUseAbility
+            (
+                HostUpgrade.UpgradeInfo.Name,
+                AlwaysUseByDefault,
+                UseROAstromechAbility,
+                descriptionLong: "Do you want to reduce the difficulty of your maneuver? If you do, gain 1 Strain token after the check difficulty step.",
+                imageHolder: HostUpgrade
+            );
+        }
+
+        private void UseROAstromechAbility(object sender, EventArgs e)
+        {
+            HostShip.AssignedManeuver.ColorComplexity = GenericMovement.ReduceComplexity(HostShip.AssignedManeuver.ColorComplexity);
+            Triggers.FinishTrigger();
+            HostShip.OnMovementFinish += GainStrain;
+        }
+
+        private void GainStrain(GenericShip ship)
+        {
+            HostShip.OnMovementFinish -= GainStrain;
+            HostShip.Tokens.AssignToken(new StrainToken(HostShip),()=>{});
+        }
+    }
+}
