86e47fdce2941d4c3cc801f1ba6b06f0e02ee6a9
Jeremy S. <baledin@gmail.com>
2026-05-30 19:12:45 -0700

    Initial implementation of PO Thanission

---
+++ Assets/Scripts/Model/Content/SecondEdition/Pilots/FO/UpsilonClassCommandShuttle/PettyOfficerThanissonEoD.cs
@@ -0,0 +1,105 @@
+using Abilities.SecondEdition;
+using Arcs;
+using Content;
+using MainPhases;
+using Ship;
+using SubPhases;
+using System.Collections.Generic;
+using Tokens;
+using Upgrade;
+using UpgradesList.SecondEdition;
+
+namespace Ship.SecondEdition.UpsilonClassCommandShuttle
+{
+    public class PettyOfficerThanissonEoD : UpsilonClassCommandShuttle
+    {
+        public PettyOfficerThanissonEoD() : base()
+        {
+            PilotInfo = new PilotCardInfo25(
+                pilotName: "Petty Officer Thanisson",
+                pilotTitle: "Evacuation of D'Qar",
+                faction: Faction.FirstOrder,
+                initiative: 1,
+                cost: 16,
+                loadoutValue: 0,
+                isLimited: true,
+                charges: 1,
+                regensCharges: 1,
+                isStandardLayout: true,
+                extraUpgradeIcons: new List<UpgradeType>()
+                {
+                    UpgradeType.Crew,
+                    UpgradeType.Sensor,
+                    UpgradeType.Tech
+                },
+                abilityType: typeof(PettyOfficerThanissonEoDAbility),
+                legality: new() { Legality.XWA }
+            );
+
+            PilotNameCanonical = "pettyofficerthanisson-evacuationofdqar";
+
+            MustHaveUpgrades.Add(typeof(GeneralHux));
+            MustHaveUpgrades.Add(typeof(AdvancedSensors));
+            MustHaveUpgrades.Add(typeof(InterferenceArray));
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class PettyOfficerThanissonEoDAbility : GenericAbility
+    {
+        private GenericShip targetShip;
+
+        public override void ActivateAbility()
+        {
+            GenericShip.OnTokenIsAssignedGlobal += CheckAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            GenericShip.OnTokenIsAssignedGlobal -= CheckAbility;
+        }
+
+        private void CheckAbility(GenericShip ship, GenericToken token)
+        {
+            if (Phases.CurrentPhase is not ActivationPhase && Phases.CurrentPhase is not CombatPhase) return;
+
+            if (token is not DepleteToken && token is not StrainToken) return;
+
+            if (HostShip.State.Charges == 0) return;
+
+            if (HostShip != ship
+                && HostShip.SectorsInfo.IsShipInSector(ship, ArcType.Front)
+                && HostShip.SectorsInfo.RangeToShipBySector(ship, ArcType.Front) <= 2)
+            {
+                targetShip = ship;
+
+                RegisterAbilityTrigger(TriggerTypes.OnTokenIsAssigned, AskAssignStress);
+            }
+        }
+
+        private void AskAssignStress(object sender, System.EventArgs e)
+        {
+            AskToUseAbility(
+                HostShip.PilotInfo.PilotName,
+                ShouldUseAbility,
+                AssignStress,
+                descriptionLong: $"Do you want to assign a Stress token to {targetShip.PilotInfo.PilotName}?",
+                imageHolder: HostShip
+            );
+        }
+
+        private bool ShouldUseAbility()
+        {
+            return Tools.IsAnotherTeam(HostShip, targetShip);
+        }
+
+        private void AssignStress(object sender, System.EventArgs e)
+        {
+            HostShip.SpendCharge();
+
+            targetShip.Tokens.AssignToken(new StressToken(targetShip), DecisionSubPhase.ConfirmDecision);
+        }
+    }
+}
---
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Tech/InterferenceArray.cs
@@ -0,0 +1,125 @@
+using Abilities.SecondEdition;
+using ActionsList;
+using Ship;
+using SubPhases;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Tokens;
+using Upgrade;
+
+namespace UpgradesList.SecondEdition
+{
+    public class InterferenceArray : GenericUpgrade
+    {
+        public InterferenceArray() : base()
+        {
+            UpgradeInfo = new UpgradeCardInfo(
+                name: "Interference Array",
+                type: UpgradeType.Tech,
+                abilityType: typeof(InterferenceArrayAbility),
+                charges: 2
+            );
+
+            IsHidden = true;
+            ImageUrl = "https://infinitearenas.com/xw2xwa/images/pilots/pettyofficerthanisson-evacuationofdqar.png";
+        }
+    }
+}
+
+namespace Abilities.SecondEdition
+{
+    public class InterferenceArrayAbility : GenericAbility
+    {
+        // After you coordinate a ship, you may spend 1 charge. If you do, assign a jam token to an enemy ship at range 0-1 of the coordinated ship.
+
+        GenericShip coordinateTarget;
+
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += RegisterAfterCoordinateAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= RegisterAfterCoordinateAbility;
+        }
+
+        private void RegisterAfterCoordinateAbility(GenericAction action)
+        {
+            if(action is CoordinateAction && HostUpgrade.State.Charges > 0)
+            {
+                coordinateTarget = action.HostShip;
+
+                RegisterAbilityTrigger(TriggerTypes.OnActionIsPerformed, AskAssignJam);
+            }
+        }
+
+        private void AskAssignJam(object sender, EventArgs e)
+        {
+            if (!HasAvailableTarget())
+            {
+                Triggers.FinishTrigger();
+                return;
+            }
+
+            AskToUseAbility(
+                descriptionShort: HostUpgrade.UpgradeInfo.Name,
+                useByDefault: AlwaysUseByDefault,
+                useAbility: SelectTargetForJam,
+                descriptionLong: $"Would you like to assign a jam token to an enemy ship at range 0-1 of {coordinateTarget.PilotInfo.PilotName}?",
+                imageHolder: HostUpgrade
+            );
+        }
+
+        private bool HasAvailableTarget()
+        {
+            return coordinateTarget.Owner.EnemyShips.Values.Any(s => coordinateTarget.GetRangeToShip(s) < 2);
+        }
+
+        private void SelectTargetForJam(object sender, EventArgs e)
+        {
+            SelectTargetForAbility(
+                selectTargetAction: AssignJam,
+                filterTargets: IsEnemyShipInRange,
+                getAiPriority: GetAiPriority,
+                subphaseOwnerPlayerNo: HostShip.Owner.PlayerNo,
+                name: HostUpgrade.UpgradeInfo.Name,
+                description: $"Select a target to assign a Jam token.",
+                imageSource: HostUpgrade,
+                onSkip: DecisionSubPhase.ConfirmDecision
+            );
+        }
+
+        private void AssignJam()
+        {
+            HostUpgrade.State.SpendCharge();
+
+            TargetShip.Tokens.AssignToken(new JamToken(TargetShip, HostShip.Owner), SelectShipSubPhase.FinishSelection);
+        }
+
+        private bool IsEnemyShipInRange(GenericShip ship)
+        {
+            return Tools.IsAnotherTeam(HostShip, ship) && coordinateTarget.GetRangeToShip(ship) < 2;
+        }
+
+        private int GetAiPriority(GenericShip ship)
+        {
+            int priority = 0;
+
+            foreach(GenericToken token in ship.Tokens.GetAllTokens())
+            {
+                priority += token switch
+                {
+                    BlueTargetLockToken => 100,
+                    FocusToken => 20,
+                    EvadeToken => 10,
+                    CalculateToken => 10,
+                    _ => 0
+                };
+            }
+
+            return priority;
+        }
+    }
+}
