723deedc6b98c72dde94c786e4d382378479bcf4
Ergiuwh <282158786+Ergiuwh@users.noreply.github.com>
2026-06-12 16:21:40 +1000

    remove unnecessary checks and use existing function

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Configuration/WingedReversal.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Configuration/WingedReversal.cs
@@ -53,9 +53,7 @@ namespace Abilities.SecondEdition
         private void CheckAbility(GenericShip ship)
         {
             if (HostUpgrade.State.Charges > 0 &&
-                ship.AssignedManeuver.Bearing == ManeuverBearing.Turn &&
-                (HostShip.AssignedManeuver.ColorComplexity == MovementComplexity.Easy ||
-                !HostShip.IsStressed))
+                ship.AssignedManeuver.Bearing == ManeuverBearing.Turn)
             {
                 RegisterAbilityTrigger(TriggerTypes.OnManeuverIsRevealed, RegisterAbility);
             }
@@ -93,13 +91,7 @@ namespace Abilities.SecondEdition
             maneuverKey = HostShip.AssignedManeuver.ToString()[..4] + "E";
             originalColor = HostShip.Maneuvers.ContainsKey(maneuverKey) ? HostShip.Maneuvers[maneuverKey] : MovementComplexity.None;
 
-            if (HostShip.AssignedManeuver.ColorComplexity != MovementComplexity.Complex) {
-                HostShip.Maneuvers[maneuverKey] = HostShip.AssignedManeuver.ColorComplexity + 1;
-            }
-            else
-            {
-                HostShip.Maneuvers[maneuverKey] = MovementComplexity.Complex;
-            }
+            HostShip.Maneuvers[maneuverKey] = GenericMovement.IncreaseComplexity(HostShip.AssignedManeuver.ColorComplexity);
 
             HostShip.SetAssignedManeuver(ShipMovementScript.MovementFromString(maneuverKey));
 
