1453df9a2131e9be5e964a7cc7be0276430ac25a
Ergiuwh <282158786+Ergiuwh@users.noreply.github.com>
2026-07-23 19:55:45 +1000

    Upload files for Pammich Nerro Goode EoD

--- Assets/Scripts/Model/Content/SecondEdition/Ships/ResistanceTransport.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Ships/ResistanceTransport.cs
@@ -4,9 +4,11 @@ using Actions;
 using ActionsList;
 using Arcs;
 using Movement;
+using Ship;
 using Ship.CardInfo;
+using SubPhases;
+using Tokens;
 using UnityEngine;
-using Upgrade;
 
 namespace Ship.SecondEdition.ResistanceTransport
 {
@@ -81,6 +83,94 @@ namespace Ship.SecondEdition.ResistanceTransport
             );
 
             ShipIconLetter = '>';
+        }
+    }
+}
+namespace Abilities.SecondEdition
+{
+    // After you perform an action, if you have fewer than 2 stress tokens, you may gain 1 stress token.
+    // If you do, another friendly small ship at range 0-1 may gain 1 deplete token to perform a Boost action.
+    class LeaveNoOneBehind : GenericAbility
+    {
+        public override void ActivateAbility()
+        {
+            HostShip.OnActionIsPerformed += RegisterLeaveNoOneBehindAbility;
+        }
+
+        public override void DeactivateAbility()
+        {
+            HostShip.OnActionIsPerformed -= RegisterLeaveNoOneBehindAbility;
+        }
+
+        private void RegisterLeaveNoOneBehindAbility(GenericAction action)
+        {
+            if (HostShip.Tokens.CountTokensByType<StressToken>() < 2)
+            {
+                RegisterAbilityTrigger(TriggerTypes.OnActionIsPerformed, AskGainStress);
+            }
+        }
+
+        private void AskGainStress(object sender, EventArgs e)
+        {
+            AskToUseAbility(
+                "Leave No One Behind",
+                NeverUseByDefault,
+                GainStressToCoordinateLike,
+                dontUseAbility: delegate { Triggers.FinishTrigger(); },
+                showAlwaysUseOption: false,
+                descriptionLong: "Do you want to gain 1 Stress Token to allow another friendly ship at range 0-1 to perform a Boost Action and gain 1 Deplete?",
+                imageHolder: HostShip,
+                showSkipButton: true
+            );
+        }
+
+        private void GainStressToCoordinateLike(object sender, EventArgs e)
+        {
+            HostShip.Tokens.AssignToken(new StressToken(HostShip), ChooseTargetShip);
+        }
+
+        private void ChooseTargetShip()
+        {
+            SelectTargetForAbility(
+                GainDepleteAndBoost,
+                AnotherFriendlySmallShipInRange,
+                AiShipPriority,
+                HostShip.Owner.PlayerNo,
+                "Leave No One Behind",
+                "Choose a ship to gain 1 Deplete Token and perform a Boost Action.",
+                HostShip,
+                true,
+                onSkip: Triggers.FinishTrigger
+            );
+        }
+
+        private void GainDepleteAndBoost()
+        {
+            TargetShip.Tokens.AssignToken(new DepleteToken(TargetShip), Boost);
+        }
+
+        private void Boost()
+        {
+            TargetShip.AskPerformFreeAction(
+                new BoostAction(),
+                Triggers.FinishTrigger,
+                descriptionShort: "Leave No One Behind",
+                descriptionLong: "Peform a free boost action",
+                imageHolder: HostShip,
+                isForced: true
+            );
+        }
+
+        private bool AnotherFriendlySmallShipInRange(GenericShip ship)
+        {
+            return FilterByTargetType(ship, new List<TargetTypes>() { TargetTypes.OtherFriendly })
+                && FilterTargetsByRange(ship, 0, 1)
+                && ship.ShipBase.Size == BaseSize.Small;
+        }
+
+        private int AiShipPriority(GenericShip ship)
+        {
+            return 1;
         }
     }
 }
