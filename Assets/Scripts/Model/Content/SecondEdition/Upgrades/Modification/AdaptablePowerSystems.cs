5139ea783cfeccf4dbbaa955737247d8b85f9853
Phil Pursglove <philpursglove@users.noreply.github.com>
2026-09-25 22:48:58 +0100

    Move SpendCharge to before RemoveToken

--- Assets/Scripts/Model/Content/SecondEdition/Upgrades/Modification/AdaptablePowerSystems.cs
+++ Assets/Scripts/Model/Content/SecondEdition/Upgrades/Modification/AdaptablePowerSystems.cs
@@ -93,9 +93,9 @@ namespace Abilities.SecondEdition
 
         private void RemoveStressTokenAndAddDeplete(object sender, EventArgs e)
         {
-            HostShip.Tokens.RemoveToken(typeof(StressToken), 
-                delegate { HostShip.Tokens.AssignToken(new DepleteToken(HostShip), DecisionSubPhase.ConfirmDecision); });
             HostUpgrade.State.SpendCharge();
+            HostShip.Tokens.RemoveToken(typeof(StressToken),
+                delegate { HostShip.Tokens.AssignToken(new DepleteToken(HostShip), DecisionSubPhase.ConfirmDecision); });
         }
 
         private void CheckAbilityAttack()
@@ -125,9 +125,9 @@ namespace Abilities.SecondEdition
 
         private void RemoveDepleteTokenAndAddStrain(object sender, EventArgs e)
         {
-            HostShip.Tokens.RemoveToken(typeof(DepleteToken), 
-                delegate {HostShip.Tokens.AssignToken(new StrainToken(HostShip), DecisionSubPhase.ConfirmDecision);});
             HostUpgrade.State.SpendCharge();
+            HostShip.Tokens.RemoveToken(typeof(DepleteToken),
+                delegate { HostShip.Tokens.AssignToken(new StrainToken(HostShip), DecisionSubPhase.ConfirmDecision); });
         }
     }
 }
